8abe1e9878605ae5e61fa7c34ad7d5d56c25bf42
z-song <zosong@126.com>
2016-10-17 18:24:55 +0800

    fix issue #55

--- src/Form.php
+++ src/Form.php
@@ -321,6 +321,7 @@ class Form
 
             switch (get_class($relation)) {
                 case \Illuminate\Database\Eloquent\Relations\BelongsToMany::class:
+                case \Illuminate\Database\Eloquent\Relations\MorphToMany::class:
                     $relation->attach($values[$name]);
                     break;
                 case \Illuminate\Database\Eloquent\Relations\HasOne::class:
@@ -395,6 +396,7 @@ class Form
 
             switch (get_class($relation)) {
                 case \Illuminate\Database\Eloquent\Relations\BelongsToMany::class:
+                case \Illuminate\Database\Eloquent\Relations\MorphToMany::class:
                     $relation->sync($prepared[$name]);
                     break;
                 case \Illuminate\Database\Eloquent\Relations\HasOne::class:
