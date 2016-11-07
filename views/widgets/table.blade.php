895c5681e6cf42f62c8fea677cf4c5f4f06e3bd6
z-song <zosong@126.com>
2016-11-07 18:15:31 +0800

    fix issue #94

--- views/widgets/table.blade.php
+++ views/widgets/table.blade.php
@@ -10,7 +10,7 @@
     @foreach($rows as $row)
     <tr>
         @foreach($row as $item)
-        <td>{{ $item }}</td>
+        <td>{!! $item !!}</td>
         @endforeach
     </tr>
     @endforeach
