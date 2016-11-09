7341335524733314027ee93f2d19765b5f1c35d4
z-song <zosong@126.com>
2016-11-09 12:57:50 +0800

    add image view for grid

--- src/Grid.php
+++ src/Grid.php
@@ -142,6 +142,13 @@ class Grid
     protected $exporter;
 
     /**
+     * View for grid to render.
+     *
+     * @var string
+     */
+    protected $view = 'admin::grid.table';
+
+    /**
      * Create a new grid instance.
      *
      * @param Eloquent $model
@@ -568,6 +575,21 @@ class Grid
     }
 
     /**
+     * Set a view to render.
+     *
+     * @param string $view
+     * @param array  $variables
+     */
+    public function view($view, $variables = [])
+    {
+        if (!empty($variables)) {
+            $this->with($variables);
+        }
+
+        $this->view = $view;
+    }
+
+    /**
      * Get the string contents of the grid view.
      *
      * @return string
@@ -580,7 +602,7 @@ class Grid
             return with(new Handle($e))->render();
         }
 
-        return view('admin::grid', $this->variables())->render();
+        return view($this->view, $this->variables())->render();
     }
 
     /**
