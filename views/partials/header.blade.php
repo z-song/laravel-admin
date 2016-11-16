8baa7cf94fad59943b8c2fc81010eb338ed6b644
z-song <zosong@126.com>
2016-11-16 14:27:06 +0800

    fix logo href issue

--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -2,7 +2,7 @@
 <header class="main-header">
 
     <!-- Logo -->
-    <a href="/admin" class="logo">
+    <a href="/{{ trim(config('admin.prefix'), '/') }}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>{{config('admin.name')}}</b></span>
         <!-- logo for regular state and mobile devices -->
