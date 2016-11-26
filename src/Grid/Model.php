4bd07579b385910ef244a2e25dce36fe508093dc
z-song <zosong@126.com>
2016-11-27 01:21:34 +0800

    fix issue #138

--- src/Grid/Model.php
+++ src/Grid/Model.php
@@ -20,9 +20,9 @@ class Model
     /**
      * Array of queries of the eloquent model.
      *
-     * @var array
+     * @var \Illuminate\Support\Collection
      */
-    protected $queries = [];
+    protected $queries;
 
     /**
      * Sort parameters of the model.
@@ -132,7 +132,7 @@ class Model
         $this->setSort();
         $this->setPaginate();
 
-        $this->queries->each(function ($query) {
+        $this->queries->unique()->each(function ($query) {
             $this->model = call_user_func_array([$this->model, $query['method']], $query['arguments']);
         });
 
@@ -208,6 +208,9 @@ class Model
         if (str_contains($this->sort['column'], '.')) {
             $this->setRelationSort($this->sort['column']);
         } else {
+
+            $this->resetOrderBy();
+
             $this->queries->push([
                 'method'    => 'orderBy',
                 'arguments' => [$this->sort['column'], $this->sort['type']],
@@ -226,7 +229,7 @@ class Model
     {
         list($relationName, $relationColumn) = explode('.', $column);
 
-        if ($this->queries->contains(function ($key, $query) use ($relationName) {
+        if ($this->queries->contains(function ($query) use ($relationName) {
             return $query['method'] == 'with' && in_array($relationName, $query['arguments']);
         })) {
             $relation = $this->model->$relationName();
@@ -236,6 +239,8 @@ class Model
                 'arguments' => $this->joinParameters($relation),
             ]);
 
+            $this->resetOrderBy();
+
             $this->queries->push([
                 'method'    => 'orderBy',
                 'arguments' => [
@@ -244,6 +249,18 @@ class Model
                 ],
             ]);
         }
+    }
+
+    /**
+     * Reset orderBy query.
+     *
+     * @return void
+     */
+    public function resetOrderBy()
+    {
+        $this->queries = $this->queries->reject(function ($query) {
+            return $query['method'] == 'orderBy';
+        });
     }
 
     /**
