be82144f810da84b36d242366d9fef1e3ec47879
z-song <zosong@126.com>
2016-12-05 23:49:18 +0800

    bind row data to column value-callback.

--- src/Grid.php
+++ src/Grid.php
@@ -352,6 +352,8 @@ class Grid
 
         $data = $this->processFilter();
 
+        Column::setOriginalGridData($data);
+
         $this->columns->map(function (Column $column) use (&$data) {
             $data = $column->fill($data);
 
