4348470739acea84672e09f33a331ad6045fbd5f
z-song <zosong@126.com>
2016-12-09 15:34:42 +0800

    fix validation issue for fields with array input.

--- views/form/error.blade.php
+++ views/form/error.blade.php
@@ -1,5 +1,15 @@
-@if($errors->has($column))
-    @foreach($errors->get($column) as $message)
-        <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> {{$message}}</label></br>
+@if(is_array($column))
+    @foreach($column as $key => $col)
+        @if($errors->has($col.$key))
+            @foreach($errors->get($col.$key) as $message)
+                <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> {{$message}}</label><br/>
+            @endforeach
+        @endif
     @endforeach
+@else
+    @if($errors->has($column))
+        @foreach($errors->get($column) as $message)
+            <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> {{$message}}</label><br/>
+        @endforeach
+    @endif
 @endif
