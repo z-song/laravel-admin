c1d8c63169168f6786e81958c2acbf245e8fa473
z-song <zosong@126.com>
2016-12-12 16:09:12 +0800

    fix back/forward issue.

--- views/index.blade.php
+++ views/index.blade.php
@@ -73,6 +73,7 @@
     $.noty.defaults.theme = 'relax';
 
     $.pjax.defaults.timeout = 5000;
+    $.pjax.defaults.maxCacheLength = 0;
     $(document).pjax('a:not(a[target="_blank"])', {
         container: '#pjax-container'
     });
