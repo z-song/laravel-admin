00c849bc73d3198cbfc26878f85d7a3ad25ef4a8
z-song <zosong@126.com>
2016-12-15 13:50:49 +0800

    Add Tags field.

---
+++ src/Form/Field/Tags.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Encore\Admin\Form\Field;
+
+use Encore\Admin\Form\Field;
+
+class Tags extends Field
+{
+    protected static $css = [
+        '/packages/admin/AdminLTE/plugins/select2/select2.min.css',
+    ];
+
+    protected static $js = [
+        '/packages/admin/AdminLTE/plugins/select2/select2.full.min.js',
+    ];
+
+    public function fill($data)
+    {
+        $relations = array_get($data, $this->column);
+
+        if (is_string($relations)) {
+            $this->value = explode(',', $relations);
+        }
+
+        $this->value = array_filter($this->value);
+    }
+
+    public function prepare($value)
+    {
+        return array_filter($value);
+    }
+
+    public function render()
+    {
+        $this->script = "$(\"#{$this->id}\").select2({
+            tags: true,
+            tokenSeparators: [',']
+        });";
+
+        return parent::render();
+    }
+}
