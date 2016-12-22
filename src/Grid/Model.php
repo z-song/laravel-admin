ba38a5232be9492c9ab89087ad3a3765a3221be5
wjazz3911 <395355364@qq.com>
2016-12-22 10:03:55 +0800

    Update Model.php

--- src/Grid/Model.php
+++ src/Grid/Model.php
@@ -257,11 +257,11 @@ class Model
 
             $this->perPage = $perPage;
         }
-        
+
         if (isset($paginate['arguments'][0])) {
             return $paginate['arguments'];
         }
-        
+
         return [$this->perPage];
     }
 
