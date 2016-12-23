e81d4e24c011d4e429175115b2d33efb1f1bfa3a
z-song <zosong@126.com>
2016-12-23 21:42:32 +0800

    add icon picker field.

---
+++ views/form/icon.blade.php
@@ -0,0 +1,17 @@
+<div class="form-group {!! !$errors->has($errorKey) ?: 'has-error' !!}">
+
+    <label for="{{$id}}" class="col-sm-2 control-label">{{$label}}</label>
+
+    <div class="col-sm-6">
+
+        @include('admin::form.error')
+
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-pencil"></i></span>
+            <input type="text" name="{{$name}}" value="{{ old($column, $value) }}" class="form-control {{$class}}"  placeholder="{{ $placeholder }}" {!! $attributes !!} />
+        </div>
+
+        @include('admin::form.help-block')
+
+    </div>
+</div>
