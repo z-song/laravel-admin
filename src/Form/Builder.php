7059680a9767b23a103bd157ba56e0e5e9cdc2e1
z-song <zosong@126.com>
2017-01-15 11:01:04 +0800

    fix issue #292

--- src/Form/Builder.php
+++ src/Form/Builder.php
@@ -283,12 +283,36 @@ class Builder
     }
 
     /**
+     * Remove reserved fields like `id` `created_at` `updated_at` in form fields.
+     *
+     * @return void
+     */
+    protected function removeReservedFields()
+    {
+        if (!$this->isMode(static::MODE_CREATE)) {
+            return;
+        }
+
+        $reservedColumns = [
+            $this->form->model()->getKeyName(),
+            $this->form->model()->getCreatedAtColumn(),
+            $this->form->model()->getUpdatedAtColumn(),
+        ];
+
+        $this->fields = $this->fields()->reject(function (Field $field) use ($reservedColumns) {
+            return in_array($field->column(), $reservedColumns);
+        });
+    }
+
+    /**
      * Render form.
      *
      * @return string
      */
     public function render()
     {
+        $this->removeReservedFields();
+
         $tabObj = $this->form->getTab();
 
         $script = <<<'SCRIPT'
