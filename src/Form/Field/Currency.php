94921e13b9a090db7c1cd444ccac47427788d6f6
Edwin Hui <xch_566@163.com>
2017-01-20 11:05:13 +0000

    Apply fixes from StyleCI

--- src/Form/Field/Currency.php
+++ src/Form/Field/Currency.php
@@ -6,45 +6,45 @@ use Encore\Admin\Form\Field;
 
 class Currency extends Field
 {
-	protected $symbol = '$';
+    protected $symbol = '$';
 
-	protected static $js = [
-		'/packages/admin/AdminLTE/plugins/input-mask/jquery.inputmask.bundle.min.js',
-	];
+    protected static $js = [
+        '/packages/admin/AdminLTE/plugins/input-mask/jquery.inputmask.bundle.min.js',
+    ];
 
-	/**
-	 * @see https://github.com/RobinHerbots/Inputmask#options
-	 *
-	 * @var array
-	 */
-	protected $options = [
-		'radixPoint'            => '.',
-		'prefix'                => '',
-		'removeMaskOnSubmit'    => true,
-	];
+    /**
+     * @see https://github.com/RobinHerbots/Inputmask#options
+     *
+     * @var array
+     */
+    protected $options = [
+        'radixPoint'            => '.',
+        'prefix'                => '',
+        'removeMaskOnSubmit'    => true,
+    ];
 
-	public function symbol($symbol)
-	{
-		$this->symbol = $symbol;
+    public function symbol($symbol)
+    {
+        $this->symbol = $symbol;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function prepare($value)
-	{
-		return (float) $value;
-	}
+    public function prepare($value)
+    {
+        return (float) $value;
+    }
 
-	public function render()
-	{
-		$options = json_encode($this->options);
+    public function render()
+    {
+        $options = json_encode($this->options);
 
-		$this->script = <<<EOT
+        $this->script = <<<EOT
 
 $('.{$this->getElementClass()}').inputmask("currency", $options);
 
 EOT;
 
-		return parent::render()->with(['symbol' => $this->symbol]);
-	}
+        return parent::render()->with(['symbol' => $this->symbol]);
+    }
 }
