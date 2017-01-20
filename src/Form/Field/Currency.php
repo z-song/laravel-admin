3172639b4b67bbb290de3f48a09bdb6791e9fb31
Edwin Hui <xch_566@163.com>
2017-01-20 19:04:17 +0800

    restore modify

--- src/Form/Field/Currency.php
+++ src/Form/Field/Currency.php
@@ -6,59 +6,45 @@ use Encore\Admin\Form\Field;
 
 class Currency extends Field
 {
-    /**
-     * digits.
-     *
-     * @var int
-     */
-    protected $digits = 2;
+	protected $symbol = '$';
 
-    protected $symbol = '$';
+	protected static $js = [
+		'/packages/admin/AdminLTE/plugins/input-mask/jquery.inputmask.bundle.min.js',
+	];
 
-    protected static $js = [
-        '/packages/admin/AdminLTE/plugins/input-mask/jquery.inputmask.bundle.min.js',
-    ];
+	/**
+	 * @see https://github.com/RobinHerbots/Inputmask#options
+	 *
+	 * @var array
+	 */
+	protected $options = [
+		'radixPoint'            => '.',
+		'prefix'                => '',
+		'removeMaskOnSubmit'    => true,
+	];
 
-    /**
-     * @see https://github.com/RobinHerbots/Inputmask#options
-     *
-     * @var array
-     */
-    protected $options = [
-        'radixPoint'            => '.',
-        'prefix'                => '',
-        'removeMaskOnSubmit'    => true,
-    ];
+	public function symbol($symbol)
+	{
+		$this->symbol = $symbol;
 
-    public function digits($number)
-    {
-        $this->digits = (int) $number;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function prepare($value)
+	{
+		return (float) $value;
+	}
 
-    public function symbol($symbol)
-    {
-        $this->symbol = $symbol;
+	public function render()
+	{
+		$options = json_encode($this->options);
 
-        return $this;
-    }
-
-    public function prepare($value)
-    {
-        return (float) $value;
-    }
-
-    public function render()
-    {
-        $options = json_encode($this->options);
-
-        $this->script = <<<EOT
+		$this->script = <<<EOT
 
 $('.{$this->getElementClass()}').inputmask("currency", $options);
 
 EOT;
 
-        return parent::render()->with(['symbol' => $this->symbol]);
-    }
+		return parent::render()->with(['symbol' => $this->symbol]);
+	}
 }
