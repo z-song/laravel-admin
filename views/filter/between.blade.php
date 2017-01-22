b7a7c8bc60df139aa27b4e7c78fb021bb866cfd8
z-song <zosong@126.com>
2017-01-22 20:40:58 +0800

    fix issue #332

--- views/filter/between.blade.php
+++ views/filter/between.blade.php
@@ -1,4 +1,4 @@
-<div class="input-group input-group-sm">
+<div class="input-group input-group-sm" style="width: 250px;">
     <span class="input-group-addon"><strong>{{$label}}</strong></span>
     <input type="text" class="form-control" placeholder="{{$label}}" name="{{$name['start']}}" value="{{ request($name['start'], $value['start']) }}" style="width: 75px">
     <span class="input-group-addon" style="border-left: 0; border-right: 0;">-</span>
