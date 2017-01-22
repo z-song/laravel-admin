7cf8a813ea854e7c530bbc9e554c352d5c45e1ac
z-song <zosong@126.com>
2017-01-23 00:11:07 +0800

    fix issue #310

--- views/filter/select.blade.php
+++ views/filter/select.blade.php
@@ -1,4 +1,4 @@
-<select class="form-control " style="width: 100%;" name="{{$name}}">
+<select class="form-control {{ $class }}" style="width: 100%;" name="{{$name}}">
     @foreach($options as $select => $option)
         <option value="{{$select}}" {{ $select == request($name, $value) ?'selected':'' }}>{{$option}}</option>
     @endforeach
