a669bc2573c515c88d11c9f23fe9f0777b2ab8a2
z-song <zosong@126.com>
2017-01-22 17:14:19 +0800

    add comments [skip ci]

--- src/Form/EmbeddedForm.php
+++ src/Form/EmbeddedForm.php
@@ -5,6 +5,50 @@ namespace Encore\Admin\Form;
 use Encore\Admin\Form;
 use Illuminate\Support\Collection;
 
+/**
+ * Class EmbeddedForm.
+ *
+ * @method Field\Text           text($column, $label = '')
+ * @method Field\Checkbox       checkbox($column, $label = '')
+ * @method Field\Radio          radio($column, $label = '')
+ * @method Field\Select         select($column, $label = '')
+ * @method Field\MultipleSelect multipleSelect($column, $label = '')
+ * @method Field\Textarea       textarea($column, $label = '')
+ * @method Field\Hidden         hidden($column, $label = '')
+ * @method Field\Id             id($column, $label = '')
+ * @method Field\Ip             ip($column, $label = '')
+ * @method Field\Url            url($column, $label = '')
+ * @method Field\Color          color($column, $label = '')
+ * @method Field\Email          email($column, $label = '')
+ * @method Field\Mobile         mobile($column, $label = '')
+ * @method Field\Slider         slider($column, $label = '')
+ * @method Field\Map            map($latitude, $longitude, $label = '')
+ * @method Field\Editor         editor($column, $label = '')
+ * @method Field\File           file($column, $label = '')
+ * @method Field\Image          image($column, $label = '')
+ * @method Field\Date           date($column, $label = '')
+ * @method Field\Datetime       datetime($column, $label = '')
+ * @method Field\Time           time($column, $label = '')
+ * @method Field\Year           year($column, $label = '')
+ * @method Field\Month          month($column, $label = '')
+ * @method Field\DateRange      dateRange($start, $end, $label = '')
+ * @method Field\DateTimeRange  datetimeRange($start, $end, $label = '')
+ * @method Field\TimeRange      timeRange($start, $end, $label = '')
+ * @method Field\Number         number($column, $label = '')
+ * @method Field\Currency       currency($column, $label = '')
+ * @method Field\HasMany        hasMany($relationName, $callback)
+ * @method Field\SwitchField    switch($column, $label = '')
+ * @method Field\Display        display($column, $label = '')
+ * @method Field\Rate           rate($column, $label = '')
+ * @method Field\Divide         divider()
+ * @method Field\Password       password($column, $label = '')
+ * @method Field\Decimal        decimal($column, $label = '')
+ * @method Field\Html           html($html, $label = '')
+ * @method Field\Tags           tags($column, $label = '')
+ * @method Field\Icon           icon($column, $label = '')
+ * @method Field\Embeds         embeds($column, $label = '')
+ *
+ */
 class EmbeddedForm
 {
     /**
--- src/Form/NestedForm.php
+++ src/Form/NestedForm.php
@@ -6,6 +6,50 @@ use Encore\Admin\Admin;
 use Encore\Admin\Form;
 use Illuminate\Support\Collection;
 
+/**
+ * Class NestedForm.
+ *
+ * @method Field\Text           text($column, $label = '')
+ * @method Field\Checkbox       checkbox($column, $label = '')
+ * @method Field\Radio          radio($column, $label = '')
+ * @method Field\Select         select($column, $label = '')
+ * @method Field\MultipleSelect multipleSelect($column, $label = '')
+ * @method Field\Textarea       textarea($column, $label = '')
+ * @method Field\Hidden         hidden($column, $label = '')
+ * @method Field\Id             id($column, $label = '')
+ * @method Field\Ip             ip($column, $label = '')
+ * @method Field\Url            url($column, $label = '')
+ * @method Field\Color          color($column, $label = '')
+ * @method Field\Email          email($column, $label = '')
+ * @method Field\Mobile         mobile($column, $label = '')
+ * @method Field\Slider         slider($column, $label = '')
+ * @method Field\Map            map($latitude, $longitude, $label = '')
+ * @method Field\Editor         editor($column, $label = '')
+ * @method Field\File           file($column, $label = '')
+ * @method Field\Image          image($column, $label = '')
+ * @method Field\Date           date($column, $label = '')
+ * @method Field\Datetime       datetime($column, $label = '')
+ * @method Field\Time           time($column, $label = '')
+ * @method Field\Year           year($column, $label = '')
+ * @method Field\Month          month($column, $label = '')
+ * @method Field\DateRange      dateRange($start, $end, $label = '')
+ * @method Field\DateTimeRange  datetimeRange($start, $end, $label = '')
+ * @method Field\TimeRange      timeRange($start, $end, $label = '')
+ * @method Field\Number         number($column, $label = '')
+ * @method Field\Currency       currency($column, $label = '')
+ * @method Field\HasMany        hasMany($relationName, $callback)
+ * @method Field\SwitchField    switch($column, $label = '')
+ * @method Field\Display        display($column, $label = '')
+ * @method Field\Rate           rate($column, $label = '')
+ * @method Field\Divide         divider()
+ * @method Field\Password       password($column, $label = '')
+ * @method Field\Decimal        decimal($column, $label = '')
+ * @method Field\Html           html($html, $label = '')
+ * @method Field\Tags           tags($column, $label = '')
+ * @method Field\Icon           icon($column, $label = '')
+ * @method Field\Embeds         embeds($column, $label = '')
+ *
+ */
 class NestedForm
 {
     const DEFAULT_KEY_NAME = '_key_';
@@ -145,6 +189,7 @@ class NestedForm
 
         $prepared = [];
 
+        /** @var Field $field */
         foreach ($this->fields as $field) {
             $columns = $field->column();
 
@@ -199,6 +244,8 @@ class NestedForm
 
             return $value;
         }
+
+        return null;
     }
 
     /**
@@ -242,6 +289,7 @@ class NestedForm
      */
     public function fill(array $data)
     {
+        /** @var Field $field */
         foreach ($this->fields() as $field) {
             $field->fill($data);
         }
@@ -256,6 +304,7 @@ class NestedForm
      */
     public function setElementClass()
     {
+        /** @var Field $field */
         foreach ($this->fields as $field) {
             $column = $field->column();
 
@@ -286,6 +335,7 @@ class NestedForm
     {
         $key = is_null($key) ? 'new_'.static::DEFAULT_KEY_NAME : $key;
 
+        /** @var Field $field */
         foreach ($this->fields as $field) {
             $column = $field->column();
 
@@ -356,6 +406,7 @@ class NestedForm
         $html = '';
         $scripts = [];
 
+        /** @var Field $field */
         foreach ($this->fields() as $field) {
             $html .= $field->render();  //when field render, will push $script to Admin
 
