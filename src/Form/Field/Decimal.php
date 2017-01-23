5ad355d56728268a2487330b172eb38f8f527ab2
z-song <zosong@126.com>
2017-01-23 16:50:02 +0800

    optimize input mask form fields.

--- src/Form/Field/Decimal.php
+++ src/Form/Field/Decimal.php
@@ -8,9 +8,21 @@ class Decimal extends Text
         '/packages/admin/AdminLTE/plugins/input-mask/jquery.inputmask.bundle.min.js',
     ];
 
+    /**
+     * @see https://github.com/RobinHerbots/Inputmask#options
+     *
+     * @var array
+     */
+    protected $options = [
+        'alias'      => 'decimal',
+        'rightAlign' => true,
+    ];
+
     public function render()
     {
-        $this->script = "$('.{$this->getElementClass()}').inputmask('decimal', { rightAlign: true});";
+        $options = json_encode($this->options);
+
+        $this->script = "$('.{$this->getElementClass()}').inputmask($options);";
 
         $this->prepend('<i class="fa fa-terminal"></i>')
             ->defaultAttribute('style', 'width: 130px');
