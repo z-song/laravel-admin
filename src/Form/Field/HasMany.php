6b1df2b6f9be246e82a51596ae3e2f221f6ec0c5
Edwin Hui <xch_566@163.com>
2017-01-25 16:48:55 +0800

    fix HasMany new index BUG.

--- src/Form/Field/HasMany.php
+++ src/Form/Field/HasMany.php
@@ -387,19 +387,19 @@ class HasMany extends Field
         /**
          * When add a new sub form, replace all element key in new sub form.
          *
-         * @example comments[new___key__][title]  => comments[new_{count}][title]
+         * @example comments[new___key__][title]  => comments[new_{index}][title]
          *
          * {count} is increment number of current sub form count.
          */
         $script = <<<EOT
 
+var index = 0;
 $('#has-many-{$this->column}').on('click', '.add', function () {
+    index++;
 
     var tpl = $('template.{$this->column}-tpl');
 
-    var count = $('.has-many-{$this->column}-forms .has-many-{$this->column}-form').size() + 1;
-
-    var template = tpl.html().replace(/{$defaultKey}/g, count);
+    var template = tpl.html().replace(/{$defaultKey}/g, index);
     $('.has-many-{$this->column}-forms').append(template);
     {$templateScript}
 });
@@ -438,14 +438,15 @@ $('#has-many-{$this->column} > .nav').off('click', 'i.close-tab').on('click', 'i
     }
     if(\$navTab.closest('li').hasClass('active')){
         \$navTab.closest('li').remove();
-        $('#has-many-{$this->column} > .nav > li:nth-child(2) > a').tab('show');
+        $('#has-many-{$this->column} > .nav > li:nth-child(1) > a').tab('show');
     }else{
         \$navTab.closest('li').remove();
     }
 });
 
-$('#has-many-{$this->column} > .nav > li.nav-tools').off('click', '.add').on('click', '.add', function(){
-    var index = $('#has-many-{$this->column} > .nav > li').size();
+var index = 0;
+$('#has-many-{$this->column} > .header').off('click', '.add').on('click', '.add', function(){
+    index++;
     var navTabHtml = $('#has-many-{$this->column} > template.nav-tab-tpl').html().replace(/{$defaultKey}/g, ''+index+'');
     var paneHtml = $('#has-many-{$this->column} > template.pane-tpl').html().replace(/{$defaultKey}/g, ''+index+'');
     $('#has-many-{$this->column} > .nav').append(navTabHtml);
