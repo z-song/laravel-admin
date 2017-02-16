dcdb78d375ec0109cca79559b3dc8c411c1a17a2
z-song <zosong@126.com>
2017-02-16 16:18:03 +0800

    scaffold develop.

--- views/helpers/scaffold.blade.php
+++ views/helpers/scaffold.blade.php
@@ -5,7 +5,7 @@
     <!-- /.box-header -->
     <div class="box-body">
 
-        <form method="post" id="scaffold">
+        <form method="post" id="scaffold" pjax-container>
 
             <div class="box-body">
 
@@ -41,16 +41,19 @@
                 </div>
 
                 <div class="form-group">
-                    <div class="col-sm-offset-1 col-sm-4">
+                    <div class="col-sm-offset-1 col-sm-11">
                         <div class="checkbox">
                             <label>
-                                <input type="checkbox" checked value="create_migration" name="create_option[]" /> make migration
+                                <input type="checkbox" checked value="migration" name="create[]" /> Create migration
                             </label>
                             <label>
-                                <input type="checkbox" checked value="create_model" name="create_option[]" /> make model
+                                <input type="checkbox" checked value="model" name="create[]" /> Create model
                             </label>
                             <label>
-                                <input type="checkbox" checked value="create_controller" name="create_option[]" /> make controller
+                                <input type="checkbox" checked value="controller" name="create[]" /> Create controller
+                            </label>
+                            <label>
+                                <input type="checkbox" checked value="migrate" name="create[]" /> Run migrate
                             </label>
                         </div>
                     </div>
@@ -72,6 +75,34 @@
                         <th>Default value</th>
                         <th>Action</th>
                     </tr>
+
+                    @if(old('fields'))
+                        @foreach(old('fields') as $index => $field)
+                            <tr>
+                                <td>
+                                    <input type="text" name="fields[{{$index}}][name]" class="form-control" placeholder="field name" value="{{$field['name']}}" />
+                                </td>
+                                <td>
+                                    <select style="width: 200px" name="fields[{{$index}}][type]">
+                                        @foreach($dbTypes as $type)
+                                            <option value="{{ $type }}" {{$field['type'] == $type ? 'selected' : '' }}>{{$type}}</option>
+                                        @endforeach
+                                    </select>
+                                </td>
+                                <td><input type="checkbox" name="fields[{{$index}}][nullable]" {{ array_get($field, 'nullable') == 'on' ? 'checked': '' }}/></td>
+                                <td>
+                                    <select style="width: 150px" name="fields[{{$index}}][key]">
+                                        {{--<option value="primary">Primary</option>--}}
+                                        <option value="" {{$field['key'] == '' ? 'selected' : '' }}>NULL</option>
+                                        <option value="unique" {{$field['key'] == 'unique' ? 'selected' : '' }}>Unique</option>
+                                        <option value="index" {{$field['key'] == 'index' ? 'selected' : '' }}>Index</option>
+                                    </select>
+                                </td>
+                                <td><input type="text" class="form-control" placeholder="default value" name="fields[{{$index}}][default]" value="{{$field['default']}}"/></td>
+                                <td><a class="btn btn-sm btn-danger table-field-remove"><i class="fa fa-trash"></i> remove</a></td>
+                            </tr>
+                        @endforeach
+                    @else
                     <tr>
                         <td>
                             <input type="text" name="fields[0][name]" class="form-control" placeholder="field name" />
@@ -95,6 +126,7 @@
                         <td><input type="text" class="form-control" placeholder="default value" name="fields[0][default]"></td>
                         <td><a class="btn btn-sm btn-danger table-field-remove"><i class="fa fa-trash"></i> remove</a></td>
                     </tr>
+                    @endif
                     </tbody>
                 </table>
 
@@ -102,92 +134,58 @@
 
                 <div class='form-inline margin' style="width: 100%">
 
-                    <div class="form-group">
-                        <label for="inputPrimaryKey">PrimaryKey name</label>
-                        <input type="text" name="primary_key" class="form-control" id="inputPrimaryKey" placeholder="PrimaryKey name" value="id" style="width: 100px;">
+
+                    <div class='form-group'>
+                        <button type="button" class="btn btn-sm btn-success" id="add-table-field"><i class="fa fa-plus"></i>&nbsp;&nbsp;Add field</button>
                     </div>
 
-                    <div class='form-group' style="margin-left: 20px;">
+                    <div class='form-group pull-right' style="margin-right: 20px; margin-top: 5px;">
                         <div class="checkbox">
                             <label>
                                 <input type="checkbox" checked name="timestamps"> Created_at & Updated_at
                             </label>
+                            &nbsp;&nbsp;
                             <label>
                                 <input type="checkbox" name="soft_deletes"> Soft deletes
                             </label>
+
                         </div>
                     </div>
 
-                    <div class='form-group pull-right' style="margin-right: 10px;">
-                        <button type="button" class="btn btn-sm btn-success" id="add-table-field"><i class="fa fa-save"></i> new</button>
+                    <div class="form-group pull-right" style="margin-right: 20px;">
+                        <label for="inputPrimaryKey">Primary key</label>
+                        <input type="text" name="primary_key" class="form-control" id="inputPrimaryKey" placeholder="Primary key" value="id" style="width: 100px;">
                     </div>
 
                 </div>
-
-                {{--<div class="row margin">--}}
-                    {{----}}
-                    {{--<div class="form-group pull-left">--}}
-
-                        {{--<div class="checkbox">--}}
-                            {{--<label>--}}
-                                {{--<input type="checkbox" checked name="use_timestamps"> Created_at & Updated_at--}}
-                            {{--</label>--}}
-                            {{--<label>--}}
-                                {{--<input type="checkbox" name="use_soft_deletes"> Soft deletes--}}
-                            {{--</label>--}}
-                        {{--</div>--}}
-
-                        {{--<a class="btn btn-sm btn-success pull-right" id="add-table-field"><i class="fa fa-save"></i> 新增</a>--}}
-
-                    {{--</div>--}}
-
-
-                {{--</div>--}}
 
                 {{--<hr />--}}
 
                 {{--<h4>Relations</h4>--}}
 
-                {{--<table class="table table-bordered">--}}
-                    {{--<tbody><tr>--}}
+                {{--<table class="table table-hover" id="model-relations">--}}
+                    {{--<tbody>--}}
+                    {{--<tr>--}}
                         {{--<th style="width: 200px">Relation name</th>--}}
                         {{--<th>Type</th>--}}
-                        {{--<th>With Pivot</th>--}}
+                        {{--<th>Related model</th>--}}
                         {{--<th>forignKey</th>--}}
                         {{--<th>OtherKey</th>--}}
+                        {{--<th>With Pivot</th>--}}
                         {{--<th>Action</th>--}}
-                    {{--</tr>--}}
-                    {{--<tr>--}}
-                        {{--<td><input type="text" class="form-control" placeholder="field name" value="id"></td>--}}
-                        {{--<td>--}}
-                            {{--<select style="width: 150px">--}}
-                                {{--<option>HasOne</option>--}}
-                                {{--<option>BelongsTo</option>--}}
-                                {{--<option>HasMany</option>--}}
-                                {{--<option>BelongsToMany</option>--}}
-                            {{--</select>--}}
-                        {{--</td>--}}
-                        {{--<td><input type="checkbox" checked></td>--}}
-                        {{--<td><input type="text" class="form-control" placeholder="default value"></td>--}}
-                        {{--<td><input type="text" class="form-control" placeholder="default value"></td>--}}
-                        {{--<td><a class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> 删除</a></td>--}}
-                    {{--</tr>--}}
-                    {{--<tr>--}}
-                        {{--<td><input type="text" class="form-control" placeholder="field name" value="id"></td>--}}
-                        {{--<td>--}}
-                            {{--<select style="width: 150px">--}}
-                                {{--<option>HasOne</option>--}}
-                                {{--<option>BelongsTo</option>--}}
-                                {{--<option>HasMany</option>--}}
-                            {{--</select>--}}
-                        {{--</td>--}}
-                        {{--<td><input type="checkbox" checked></td>--}}
-                        {{--<td><input type="text" class="form-control" placeholder="default value"></td>--}}
-                        {{--<td><input type="text" class="form-control" placeholder="default value"></td>--}}
-                        {{--<td><a class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> 删除</a></td>--}}
                     {{--</tr>--}}
                     {{--</tbody>--}}
                 {{--</table>--}}
+
+                {{--<hr style="margin-top: 0;"/>--}}
+
+                {{--<div class='form-inline margin' style="width: 100%">--}}
+
+                    {{--<div class='form-group'>--}}
+                        {{--<button type="button" class="btn btn-sm btn-success" id="add-model-relation"><i class="fa fa-plus"></i>&nbsp;&nbsp;Add relation</button>--}}
+                    {{--</div>--}}
+
+                {{--</div>--}}
 
             </div>
             <!-- /.box-body -->
@@ -199,7 +197,6 @@
 
             <!-- /.box-footer -->
         </form>
-
 
     </div>
 
@@ -220,13 +217,32 @@
         <td><input type="checkbox" name="fields[__index__][nullable]" /></td>
         <td>
             <select style="width: 150px" name="fields[__index__][key]">
-                <option selected>NULL</option>
+                <option value="" selected>NULL</option>
                 <option value="unique">Unique</option>
                 <option value="index">Index</option>
             </select>
         </td>
         <td><input type="text" class="form-control" placeholder="default value" name="fields[__index__][default]"></td>
         <td><a class="btn btn-sm btn-danger table-field-remove"><i class="fa fa-trash"></i> remove</a></td>
+    </tr>
+</template>
+
+<template id="model-relation-tpl">
+    <tr>
+        <td><input type="text" class="form-control" placeholder="relation name" value=""></td>
+        <td>
+            <select style="width: 150px">
+                <option value="HasOne" selected>HasOne</option>
+                <option value="BelongsTo">BelongsTo</option>
+                <option value="HasMany">HasMany</option>
+                <option value="BelongsToMany">BelongsToMany</option>
+            </select>
+        </td>
+        <td><input type="text" class="form-control" placeholder="related model"></td>
+        <td><input type="text" class="form-control" placeholder="default value"></td>
+        <td><input type="text" class="form-control" placeholder="default value"></td>
+        <td><input type="checkbox" /></td>
+        <td><a class="btn btn-sm btn-danger model-relation-remove"><i class="fa fa-trash"></i> remove</a></td>
     </tr>
 </template>
 
@@ -237,17 +253,25 @@ $(function () {
     $('input[type=checkbox]').iCheck({checkboxClass:'icheckbox_minimal-blue'});
     $('select').select2();
 
-    var fields_count = 1;
-
     $('#add-table-field').click(function (event) {
-        $('#table-fields tbody').append($('#table-field-tpl').html().replace(/__index__/g, fields_count));
+        $('#table-fields tbody').append($('#table-field-tpl').html().replace(/__index__/g, $('#table-fields tr').length - 1));
         $('select').select2();
         $('input[type=checkbox]').iCheck({checkboxClass:'icheckbox_minimal-blue'});
-
-        fields_count++;
     });
 
     $('#table-fields').on('click', '.table-field-remove', function(event) {
+        $(event.target).closest('tr').remove();
+    });
+
+    $('#add-model-relation').click(function (event) {
+        $('#model-relations tbody').append($('#model-relation-tpl').html().replace(/__index__/g, $('#model-relations tr').length - 1));
+        $('select').select2();
+        $('input[type=checkbox]').iCheck({checkboxClass:'icheckbox_minimal-blue'});
+
+        relation_count++;
+    });
+
+    $('#model-relations').on('click', '.model-relation-remove', function(event) {
         $(event.target).closest('tr').remove();
     });
 
