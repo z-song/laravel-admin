83a030cd998eaa880558c095c5c17ef73dec5dab
z-song <zosong@126.com>
2017-02-18 01:23:09 +0800

    fix grid paginate issue.

--- src/Grid/Model.php
+++ src/Grid/Model.php
@@ -311,6 +311,10 @@ class Model
             $this->perPage = (int) $perPage;
         }
 
+        if (isset($paginate['arguments'][0])) {
+            return $paginate['arguments'];
+        }
+
         return [$this->perPage];
     }
 
