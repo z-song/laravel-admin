2436ef28e34beab2b91e03cb0313ae65c73a0c8d
z-song <zosong@126.com>
2017-02-24 16:40:48 +0800

    add captcha form field.

---
+++ src/Form/Field/Captcha.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Encore\Admin\Form\Field;
+
+use Encore\Admin\Form;
+
+class Captcha extends Text
+{
+    protected $rules = 'required|captcha';
+
+    protected $view = 'admin::form.captcha';
+
+    public function __construct($column, $arguments = [])
+    {
+        if (!class_exists(\Mews\Captcha\Captcha::class)) {
+            throw new \Exception('To use captcha field, please install [mews/captcha] first.');
+        }
+
+        $this->column = '__captcha__';
+        $this->label = trans('admin::lang.captcha');
+    }
+
+    public function setForm(Form $form = null)
+    {
+        $this->form = $form;
+
+        $this->form->ignore($this->column);
+
+        return $this;
+    }
+
+    public function render()
+    {
+        $this->script = <<<EOT
+
+$('#{$this->column}-captcha').click(function () {
+    $(this).attr('src', $(this).attr('src')+'?'+Math.random());
+});
+
+EOT;
+
+        return parent::render();
+    }
+}
