8a3106ce0709984c00c1704dd5fb401b3596757c
z-song <zosong@126.com>
2017-02-24 17:04:50 +0800

    optimize success and toastr

--- views/partials/success.blade.php
+++ views/partials/success.blade.php
@@ -1,6 +1,8 @@
 @if(Session::has('success'))
-    @php $success = Session::get('success');@endphp
-    <script>
-        toastr.success('{{ array_get($success->get('message'), 0) }}');
-    </script>
+    <?php $success = Session::get('success');?>
+    <div class="alert alert-success alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-check"></i>{{ array_get($success->get('title'), 0) }}</h4>
+        <p>{!!  array_get($success->get('message'), 0) !!}</p>
+    </div>
 @endif
