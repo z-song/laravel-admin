d1f5733de293c8102cea0cc00bcd0791fa6ba35e
z-song <zosong@126.com>
2017-03-02 18:16:39 +0800

    fix range filter issue

--- views/filter/betweenDatetime.blade.php
+++ views/filter/betweenDatetime.blade.php
@@ -1,6 +1,6 @@
 <div class="input-group input-group-sm">
     <span class="input-group-addon"><strong>{{$label}}</strong></span>
-    <input type="text" class="form-control" id="{{$id['start']}}" placeholder="{{$label}}" name="{{$name['start']}}" value="{{ request($name['start'], $value['start']) }}">
+    <input type="text" class="form-control" id="{{$id['start']}}" placeholder="{{$label}}" name="{{$name['start']}}" value="{{ request($name['start'], array_get($value, 'start')) }}">
     <span class="input-group-addon" style="border-left: 0; border-right: 0;">-</span>
-    <input type="text" class="form-control" id="{{$id['end']}}" placeholder="{{$label}}" name="{{$name['end']}}" value="{{ request($name['end'], $value['end']) }}">
+    <input type="text" class="form-control" id="{{$id['end']}}" placeholder="{{$label}}" name="{{$name['end']}}" value="{{ request($name['end'], array_get($value, 'end')) }}">
 </div>
