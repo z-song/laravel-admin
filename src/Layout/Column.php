4c08ba798589f5006bcba6aa3dc5ee0eb82a5083
z-song <zosong@126.com>
2017-03-03 10:23:36 +0800

    optimize layout.

--- src/Layout/Column.php
+++ src/Layout/Column.php
@@ -6,10 +6,21 @@ use Illuminate\Contracts\Support\Renderable;
 
 class Column implements Buildable
 {
+    /**
+     * @var int
+     */
     protected $width = 12;
 
+    /**
+     * @var array
+     */
     protected $contents = [];
 
+    /**
+     * Column constructor.
+     * @param $content
+     * @param int $width
+     */
     public function __construct($content, $width = 12)
     {
         if ($content instanceof \Closure) {
@@ -21,11 +32,47 @@ class Column implements Buildable
         $this->width = $width;
     }
 
+    /**
+     * Append content to column.
+     *
+     * @param $content
+     *
+     * @return $this
+     */
     public function append($content)
     {
         $this->contents[] = $content;
+
+        return $this;
     }
 
+    /**
+     * Add a row for column.
+     *
+     * @param $content
+     *
+     * @return Column
+     */
+    public function row($content)
+    {
+        $row = new Row();
+
+        call_user_func($content, $row);
+
+        ob_start();
+
+        $row->build();
+
+        $contents = ob_get_contents();
+
+        ob_end_clean();
+
+        return $this->append($contents);
+    }
+
+    /**
+     * Build column html.
+     */
     public function build()
     {
         $this->startColumn();
@@ -41,11 +88,17 @@ class Column implements Buildable
         $this->endColumn();
     }
 
+    /**
+     * Start column.
+     */
     protected function startColumn()
     {
         echo "<div class=\"col-md-{$this->width}\">";
     }
 
+    /**
+     * End column.
+     */
     protected function endColumn()
     {
         echo '</div>';
