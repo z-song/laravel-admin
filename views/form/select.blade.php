1ede8c35788cfceaf75eb37b25674a3311d0be57
Jon Phipps <jphipps@madcreek.com>
2017-03-06 12:35:47 -0500

    Add support for placeholder to form select

--- views/form/select.blade.php
+++ views/form/select.blade.php
@@ -8,7 +8,8 @@
 
         <input type="hidden" name="{{$name}}"/>
 
-        <select class="form-control {{$class}}" style="width: 100%;" name="{{$name}}" {!! $attributes !!} >
+        <select class="form-control {{$class}}" style="width: 100%;" name="{{$name}}" data-placeholder="{{ $placeholder }}" {!! $attributes !!} >
+            <option></option>
             @foreach($options as $select => $option)
                 <option value="{{$select}}" {{ $select == old($column, $value) ?'selected':'' }}>{{$option}}</option>
             @endforeach
