df1fb852c2440e6fc3cd090ca4cd8d40076869a4
Ladel <infobox12@mail.ru>
2017-03-14 17:46:18 +0500

    Apply fixes from Travis-CI and StyleCI

--- src/Form/Field/File.php
+++ src/Form/Field/File.php
@@ -44,7 +44,7 @@ class File extends Field
      */
     public function getValidator(array $input)
     {
-        /**
+        /*
          * If has original value, means the form is in edit mode,
          * then remove required rule from rules.
          */
--- src/Form/Field/MultipleFile.php
+++ src/Form/Field/MultipleFile.php
@@ -73,8 +73,8 @@ class MultipleFile extends Field
         $rules = $input = [];
 
         foreach ($value as $key => $file) {
-            $rules[$this->column . $key] = $this->getRules();
-            $input[$this->column . $key] = $file;
+            $rules[$this->column.$key] = $this->getRules();
+            $input[$this->column.$key] = $file;
         }
 
         return [$rules, $input];
