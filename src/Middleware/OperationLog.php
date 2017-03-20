a09ede7afe5dc74e7b3e89b907d3bd7f8d4805b0
Slava Vishnyakov <bomboze@gmail.com>
2017-03-20 09:38:36 +0300

    StyleCI

--- src/Middleware/OperationLog.php
+++ src/Middleware/OperationLog.php
@@ -33,10 +33,11 @@ class OperationLog
     }
 
     /**
-     * Replace passwords with stars in operation log
+     * Replace passwords with stars in operation log.
      * @see https://github.com/z-song/laravel-admin/issues/625
      *
      * @param string $stringToLog
+     *
      * @return string
      */
     public function hidePasswords($stringToLog)
