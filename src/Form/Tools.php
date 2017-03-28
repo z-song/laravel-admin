700130220dfc44bf8fb0c14dc8ed6f78271b4040
edwinhuish <xch_566@163.com>
2017-03-28 11:56:29 +0800

    fix listButton resource url

--- src/Form/Tools.php
+++ src/Form/Tools.php
@@ -68,19 +68,19 @@ EOT;
 
     public function listButton()
     {
-        $resource = url()->previous();
+        $preUrl = url()->previous();
 
-        if(!$resource || url()->current() === $resource){
+	    $slice = Str::contains($this->form->getResource(0), '/edit') ? null : -1;
 
-            $slice = Str::contains($this->form->getResource(0), '/edit') ? null : -1;
-            $resource = $this->form->getResource($slice);
-        }
+	    $baseListUrl = request()->root().$this->form->getResource($slice);
+
+	    $resource = (stripos($preUrl, $baseListUrl.'?', 0) === false) ?  $baseListUrl : $preUrl;
 
         $text = trans('admin::lang.list');
 
         return <<<EOT
 <div class="btn-group pull-right" style="margin-right: 10px">
-    <a href="$resource" class="btn btn-sm btn-default btn-success-redirect"><i class="fa fa-list"></i>&nbsp;$text</a>
+    <a href="$resource" class="btn btn-sm btn-default  btn-success-redirect"><i class="fa fa-list"></i>&nbsp;$text</a>
 </div>
 EOT;
     }
