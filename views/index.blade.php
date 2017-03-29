772a5bf610c3e1b8dca46efc727964e5c6824c41
Titouan B <nightbringer4ever@gmail.com>
2017-03-29 16:40:15 +0200

    Add charset utf-8
    Add charset utf-8 to fix broken characters in js AlertBox

--- views/index.blade.php
+++ views/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{ Admin::title() }}</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
