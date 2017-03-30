c7c5add98ba9b37a3658217a60983c37f2eb5ea1
z-song <zosong@126.com>
2017-03-30 16:27:17 +0800

    optimize model-grid row

--- src/Grid.php
+++ src/Grid.php
@@ -577,12 +577,8 @@ class Grid
      */
     protected function buildRows(array $data)
     {
-        $this->rows = collect($data)->map(function ($val, $key) {
-            $row = new Row($key, $val);
-
-            $row->setKeyName($this->keyName);
-
-            return $row;
+        $this->rows = collect($data)->map(function ($model, $number) {
+            return new Row($number, $model);
         });
 
         if ($this->rowsCallback) {
