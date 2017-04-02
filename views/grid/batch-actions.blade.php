8b6be00ec6cf56d3008d739f37c2adbfdd0ae637
edwinhuish <xch_566@163.com>
2017-04-02 19:27:48 +0800

    change action batch style

--- views/grid/batch-actions.blade.php
+++ views/grid/batch-actions.blade.php
@@ -1,11 +1,9 @@
 <input type="checkbox" class="grid-select-all" />&nbsp;
 
 <div class="btn-group">
-    <a class="btn btn-sm btn-default">  {{ trans('admin::lang.action') }}</a>
-    <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown">
-        <span class="caret"></span>
-        <span class="sr-only">Toggle Dropdown</span>
-    </button>
+    <a class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+        {{ trans('admin::lang.action') }} <span class="fa fa-caret-down"></span>
+    </a>
     <ul class="dropdown-menu" role="menu">
         @foreach($actions as $action)
             <li><a href="#" class="grid-batch-{{ $action['id'] }}">{{ $action['title'] }}</a></li>
