1419203aec93f02c178707de49db154704749bc8
ebtech@ebtech.pl <pcpi@debian>
2017-04-07 18:44:07 +0200

    add polish lang

---
+++ lang/pl/lang.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    'online'        => 'Online',
+    'login'         => 'Login',
+    'logout'        => 'Logout',
+    'name'          => 'Nazwa',
+    'username'      => 'Użytkownik',
+    'password'      => 'Hasło',
+    'remember_me'   => 'Zapamiętaj mnie',
+
+    'list'          => 'Lista',
+    'new'           => 'Nowy',
+    'create'        => 'Utwórz',
+    'delete'        => 'Usuń',
+    'edit'          => 'Edytuj',
+    'view'          => 'Podgląd',
+    'reset'         => 'Resetuj',
+    'export'        => 'Eksportuj',
+    'batch_delete'  => 'Batch delete',
+    'save'          => 'Zapisz',
+    'refresh'       => 'Odśwież',
+    'order'         => 'Sortuj',
+    'expand'        => 'Rozwiń',
+    'collapse'      => 'Schowaj',
+    'filter'        => 'Filtr',
+    'close'         => 'Zamknij',
+    'show'          => 'Pokaż',
+    'items'         => 'element',
+
+    'action'           => 'Akcja',
+    'title'            => 'Tytuł',
+    'description'      => 'Opis',
+    'back_to_list'     => 'Cofnij do listy',
+    'submit'           => 'Wyślij',
+    'menu'             => 'Menu',
+    'input'            => 'Input',
+    'succeeded'        => 'Sukces',
+    'failed'           => 'błąd',
+    'delete_confirm'   => 'Czy na pewno chcesz usunąć element ?',
+    'delete_succeeded' => 'Element został usunięty !',
+    'delete_failed'    => 'Kasowanie się nie powiodło !',
+
+    'choose'        => 'Zamień',
+    'choose_file'   => 'Zamień plik',
+    'choose_image'  => 'Zamień obrazek',
+
+    'more'          => 'Więcej',
+    'deny'          => 'Brak dostępu',
+
+    'administrator' => 'Administrator',
+    'roles'         => 'Role',
+    'permissions'   => 'Zabespieczenia',
+    'slug'          => 'adres',
+
+    'created_at'    => 'Created At',
+    'updated_at'    => 'Updated At',
+
+    'alert'         => 'Alarm',
+
+    'parent_id'     => 'Parent',
+    'icon'          => 'Icona',
+    'uri'           => 'URI',
+
+    'operation_log'       => 'Log operacji',
+    'parent_select_error' => 'Parent select error',
+];
