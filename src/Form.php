6e072428b78eab36562cefef105ffd9e008bd30c
Binh An Tran <tran.binhan@gmail.com>
2017-04-17 10:59:41 +0200

    Add submitted callback.

--- src/Form.php
+++ src/Form.php
@@ -86,6 +86,13 @@ class Form
     protected $builder;
 
     /**
+     * Submitted callback.
+     *
+     * @var Closure
+     */
+    protected $submitted;
+
+    /**
      * Saving callback.
      *
      * @var Closure
@@ -384,6 +391,11 @@ class Form
      */
     protected function prepare($data = [])
     {
+
+        if (($response = $this->callSubmitted()) instanceof Response) {
+            return $response;
+        }
+
         $this->inputs = $this->removeIgnoredFields($data);
 
         if (($response = $this->callSaving()) instanceof Response) {
@@ -435,6 +447,18 @@ class Form
         }
 
         return $relations;
+    }
+
+    /**
+     * Call submitted callback.
+     *
+     * @return mixed
+     */
+    protected function callSubmitted()
+    {
+        if ($this->submitted instanceof Closure) {
+            return call_user_func($this->submitted, $this);
+        }
     }
 
     /**
@@ -786,6 +810,18 @@ class Form
         }
 
         return Arr::isAssoc($first);
+    }
+
+    /**
+     * Set submitted callback.
+     *
+     * @param Closure $callback
+     *
+     * @return void
+     */
+    public function submitted(Closure $callback)
+    {
+        $this->submitted = $callback;
     }
 
     /**
