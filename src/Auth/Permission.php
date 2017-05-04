a06c3f59e78f667d5446fbc0b5ffec1e36d83d1e
kotroczo.mate <kotroczo.mate@gmail.com>
2017-05-04 11:31:08 +0200

    hotfix: Cannot redeclare Encore\Admin\Auth\Permission::isAdministrator()

--- src/Auth/Permission.php
+++ src/Auth/Permission.php
@@ -89,14 +89,4 @@ class Permission
     {
         return Auth::guard('admin')->user()->isRole('administrator');
     }
-
-    /**
-     * If current user is administrator.
-     *
-     * @return mixed
-     */
-    public static function isAdministrator()
-    {
-        return Auth::guard('admin')->user()->isRole('administrator');
-    }
 }
