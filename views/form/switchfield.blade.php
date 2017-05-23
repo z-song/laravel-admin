e867fd62ec1ce1f4a07e93175f8203bd7b67c707
Бабичев Максим <maksim.babichev95@gmail.com>
2017-05-23 20:39:51 +0300

    Update switchfield.blade.php

--- views/form/switchfield.blade.php
+++ views/form/switchfield.blade.php
@@ -6,8 +6,8 @@
 
         @include('admin::form.error')
 
-        <input type="checkbox" class="{{$class}} la_checkbox" {{ old($column, $value) == 'on' ? 'checked' : '' }} {!! $attributes !!} />
-        <input type="hidden" class="{{$class}}" name="{{$name}}" class="" value="{{ old($column, $value) }}" />
+        <input type="checkbox" class="{{$class}} la_checkbox" data-name="{{$name}}" {{ old($column, $value) == 'on' ? 'checked' : '' }} {!! $attributes !!} />
+        <input type="hidden" class="{{$class}}" name="{{$name}}" value="{{ old($column, $value) }}" />
 
         @include('admin::form.help-block')
 
