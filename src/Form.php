7e3c90e8ee1b32c238c13b16f6308eade66f14a9
Runnerlee <runnerleer@gmail.com>
2017-06-19 12:35:56 +0800

    update Form::setFieldValue()

--- src/Form.php
+++ src/Form.php
@@ -936,6 +936,16 @@ class Form
 
         $data = $this->model->toArray();
 
+        if ($this->model::$snakeAttributes) {
+            foreach ($this->model->getRelations() as $key => $relation) {
+                if ($key === $snake = Str::snake($key)) {
+                    continue;
+                }
+                $data[$key] = $data[$snake];
+                unset($data[$snake]);
+            }
+        }
+
         $this->builder->fields()->each(function (Field $field) use ($data) {
             $field->fill($data);
         });
