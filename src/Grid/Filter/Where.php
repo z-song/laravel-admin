bf346cf201aba6d28e0a4aeb59a282fda1efa213
z-song <zosong@126.com>
2017-09-27 23:12:25 +0800

    fix issue #1102

--- src/Grid/Filter/Where.php
+++ src/Grid/Filter/Where.php
@@ -62,11 +62,7 @@ class Where extends AbstractFilter
     {
         $value = array_get($inputs, $this->column ?: static::getQueryHash($this->where, $this->label));
 
-        if (is_array($value)) {
-            $value = array_filter($value);
-        }
-
-        if (is_null($value) || empty($value)) {
+        if (is_null($value)) {
             return;
         }
 
