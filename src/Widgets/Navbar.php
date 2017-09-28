54a603ea3beb4bee7d536d3e1cc644a6a76c8119
z-song <zosong@126.com>
2017-09-28 15:20:23 +0800

    Support add elements to top navbar.

--- src/Widgets/Navbar.php
+++ src/Widgets/Navbar.php
@@ -4,36 +4,78 @@ namespace Encore\Admin\Widgets;
 
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Support\Collection;
 
 class Navbar implements Renderable
 {
-    protected $items;
+    /**
+     * @var array
+     */
+    protected $elements = [];
 
+    /**
+     * Navbar constructor.
+     */
     public function __construct()
     {
-        $this->items = new Collection();
+        $this->elements = [
+            'left'   => collect(),
+            'right'  => collect(),
+        ];
     }
 
-    public function add($item)
+    /**
+     * @param $element
+     * @return $this
+     */
+    public function left($element)
     {
-        $this->items->push($item);
+        $this->elements['left']->push($element);
 
         return $this;
     }
 
-    public function render()
+    /**
+     * @param $element
+     * @return $this
+     */
+    public function right($element)
     {
-        return $this->items->reverse()->map(function ($item) {
-            if ($item instanceof Htmlable) {
-                return $item->toHtml();
+        $this->elements['right']->push($element);
+
+        return $this;
+    }
+
+    /**
+     * @param $element
+     * @return Navbar
+     *
+     * @deprecated
+     */
+    public function add($element)
+    {
+        return $this->right($element);
+    }
+
+    /**
+     * @param string $part
+     * @return mixed
+     */
+    public function render($part = 'right')
+    {
+        if (!isset($this->elements[$part]) || $this->elements[$part]->isEmpty()) {
+            return '';
+        }
+
+        return $this->elements[$part]->map(function ($element) {
+            if ($element instanceof Htmlable) {
+                return $element->toHtml();
             }
 
-            if ($item instanceof Renderable) {
-                return $item->render();
+            if ($element instanceof Renderable) {
+                return $element->render();
             }
 
-            return (string) $item;
+            return (string) $element;
         })->implode('');
     }
 }
