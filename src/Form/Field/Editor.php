5267463278794d0bc2caf6b4c0eb81d9a03d6c4c
miadz <miada48@gmail.com>
2017-10-24 18:03:38 +0330

    set language and direction for ckeditor
    set language and direction for ckeditor, default is config('app.locale')

--- src/Form/Field/Editor.php
+++ src/Form/Field/Editor.php
@@ -10,9 +10,43 @@ class Editor extends Field
         '//cdn.ckeditor.com/4.5.10/standard/ckeditor.js',
     ];
 
+    public function __construct($column, array $arguments = [])
+    {
+        parent::__construct($column, $arguments);
+
+        $this->options(['contentsLangDirection' => 'ltr']);
+        $this->options(['language' => config('app.locale', 'en')]);
+
+    }
+
+    /**
+     * set `direction` .
+     * https://docs-old.ckeditor.com/ckeditor_api/symbols/CKEDITOR.config.html#.contentsLangDirection
+     * 'ui' – indicates that content direction will be the same as the user interface language direction;
+     * 'ltr' – for Left-To-Right language (like English);
+     * 'rtl' – for Right-To-Left languages (like Arabic).
+     * @param string $dir
+     * @return $this
+     */
+    public function dir($dir)
+    {
+        return $this->options(['contentsLangDirection' => $dir]);
+    }
+
+    /**
+     * set language for editor
+     * @param string $dir
+     * @return $this
+     */
+    public function lang($dir)
+    {
+        return $this->options(['language' => $dir]);
+    }
+
     public function render()
     {
-        $this->script = "CKEDITOR.replace('{$this->column}');";
+        $options = json_encode($this->options);
+        $this->script = "CKEDITOR.replace('{$this->column}', $options);";
 
         return parent::render();
     }
