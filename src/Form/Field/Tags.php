618a290f0e043282c7a960625f2a389af229a117
miadz <miada48@gmail.com>
2017-10-24 18:17:55 +0330

    ajax function for load for tags and auto set direction and language for select2
    ajax function for load for tags and auto set direction and language for select2

--- src/Form/Field/Tags.php
+++ src/Form/Field/Tags.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Arr;
 class Tags extends Field
 {
     protected $value = [];
+    protected $direction = "ltr";
 
     protected static $css = [
         '/vendor/laravel-admin/AdminLTE/plugins/select2/select2.min.css',
@@ -25,7 +26,18 @@ class Tags extends Field
             $this->value = explode(',', $this->value);
         }
 
-        $this->value = array_filter((array) $this->value);
+        $this->value = array_filter((array)$this->value);
+    }
+
+    /**
+     * Set direction setting of select2.
+     *
+     */
+    public function dir($dir = 'ltr')
+    {
+        $this->direction = $dir;
+
+        return $this;
     }
 
     public function prepare($value)
@@ -37,12 +49,72 @@ class Tags extends Field
         return $value;
     }
 
+    /**
+     * Load options from ajax results.
+     *
+     * @param string $url
+     * @param        $idField
+     * @param        $textField
+     *
+     * @return $this
+     */
+    public function ajax($url, $idField = 'id', $textField = 'text')
+    {
+        $this->script = <<<EOT
+
+$("{$this->getElementClassSelector()}").select2({
+  dir: "$this->direction",
+  language : "$this->local",
+  tags: true,
+  multiple: true,
+  ajax: {
+    url: "$url",
+    dataType: 'json',
+    delay: 250,
+    data: function (params) {
+      return {
+        q: params.term,
+        page: params.page
+      };
+    },
+    processResults: function (data, params) {
+      params.page = params.page || 1;
+
+      return {
+        results: $.map(data.data, function (d) {
+                   d.id = d.$idField;
+                   d.text = d.$textField;
+                   return d;
+                }),
+        pagination: {
+          more: data.next_page_url
+        }
+      };
+    },
+    cache: true
+  },
+  minimumInputLength: 1,
+  escapeMarkup: function (markup) {
+      return markup;
+  }
+});
+
+EOT;
+
+        return $this;
+    }
+
     public function render()
     {
-        $this->script = "$(\"{$this->getElementClassSelector()}\").select2({
+        if (empty($this->script)) {
+            $this->script = "$(\"{$this->getElementClassSelector()}\").select2({
+            dir: \"$this->direction\",
+            language : \"$this->local\",
             tags: true,
             tokenSeparators: [',']
         });";
+
+        }
 
         return parent::render();
     }
