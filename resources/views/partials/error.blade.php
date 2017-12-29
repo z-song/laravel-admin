0c643e55fe0dc5addc28fa9995db696073cbf0af
叶剑峰 <yejianfeng@didichuxing.com>
2017-12-29 11:06:54 +0800

    在laravel的errors里面增加一个error的key的messageBag提供给自定义的错误提醒

--- resources/views/partials/error.blade.php
+++ resources/views/partials/error.blade.php
@@ -1,20 +1,19 @@
-@if(Session::has('error') || Session::has('errors'))
+@if(Session::has('error'))
     <?php $error = Session::get('error');?>
-    <?php $errors = Session::get('errors');?>
     <div class="alert alert-danger alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        @if (!empty($error))
         <h4><i class="icon fa fa-ban"></i>{{ array_get($error->get('title'), 0) }}</h4>
-        @endif
-        @if (!empty($error))
         <p>{!!  array_get($error->get('message'), 0) !!}</p>
-        @endif
-        @if (!empty($errors))
-            @foreach($errors->getBags() as $messageBag)
-                @foreach($messageBag->toArray() as $message)
-                    <p>{!!  array_get($message, 0) !!}</p>
-                @endforeach
-            @endforeach
-        @endif
     </div>
+@elseif (Session::has('errors'))
+    <?php $errors = Session::get('errors');?>
+    @if ($errors->hasBag('error'))
+      <div class="alert alert-danger alert-dismissable">
+
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        @foreach($errors->getBag("error")->toArray() as $message)
+            <p>{!!  array_get($message, 0) !!}</p>
+        @endforeach
+      </div>
+    @endif
 @endif
