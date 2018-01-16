21981cbd46286642d6637af4084771c88c24b771
yulinking <minest@yahoo.cn>
2018-01-16 21:21:02 +0800

    Update Label.php
    fix label too long bug, add newline parameter, if label number more then newline, add <br/>

--- src/Grid/Displayers/Label.php
+++ src/Grid/Displayers/Label.php
@@ -6,14 +6,20 @@ use Illuminate\Contracts\Support\Arrayable;
 
 class Label extends AbstractDisplayer
 {
-    public function display($style = 'success')
+    //fixed by kingstudio at 2018-1-16
+    //fix label too long bug, add newline parameter, if label number more then newline, add <br/>
+    //
+    public function display($style = 'success', $newline = 5)
     {
         if ($this->value instanceof Arrayable) {
             $this->value = $this->value->toArray();
         }
 
-        return collect((array) $this->value)->map(function ($name) use ($style) {
-            return "<span class='label label-{$style}'>$name</span>";
-        })->implode('&nbsp;');
+        return collect((array) $this->value)->map(function ($name, $key) use ($style, $newline) {
+            if (($key + 1) % $newline == 0)
+                return "<span class='label label-{$style}'>$name</span><br/>";
+            else
+                return "<span class='label label-{$style}'>$name</span>&nbsp;";
+        })->implode('');
     }
 }
