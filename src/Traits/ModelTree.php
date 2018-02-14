58eccf4448f384f2b66da5b5b4856ee1a8985790
Vlad Kucherov <vlad@vladk.co.il>
2018-02-14 21:00:31 +0200

    Fixes model tree parent bug
    Fixes model tree matching null parent column with a NULL primary key when creating a new model instance.
    When creating an entry through the form of a model that uses ModelTree,
    When the form is saving, it tries to validate whether the parent_id is the same model primary id.
    But because a newly created model instance has primary id as NULL, it matches with a null parent id (when none was selected).
    And those, throwing an error.
    
    Basically, the change also validates now if there is a value, which is also nice.

--- src/Traits/ModelTree.php
+++ src/Traits/ModelTree.php
@@ -293,7 +293,7 @@ trait ModelTree
         static::saving(function (Model $branch) {
             $parentColumn = $branch->getParentColumn();
 
-            if (Request::has($parentColumn) && Request::input($parentColumn) == $branch->getKey()) {
+            if (Request::get($parentColumn) && Request::input($parentColumn) == $branch->getKey()) {
                 throw new \Exception(trans('admin.parent_select_error'));
             }
 
