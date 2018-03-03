0fe7929eb8863feb531cdf99b6e649c670bd22b9
João Serra <joaoserra@Joaos-MBP.lan>
2018-03-03 02:35:05 +0000

    Fix failing test

--- tests/FileUploadTest.php
+++ tests/FileUploadTest.php
@@ -90,7 +90,7 @@ class FileUploadTest extends TestCase
             ->seeElement('input[name=file6]')
 //            ->seeInElement('a[href="/admin/files"]', 'List')
             ->seeInElement('button[type=reset]', 'Reset')
-            ->seeInElement('button[type=submit]', 'Submit');
+            ->seeInElement('button[type=submit]', 'Save');
 
         $this->attach(__DIR__.'/RolesTest.php', 'file3')
             ->attach(__DIR__.'/MenuTest.php', 'file4')
