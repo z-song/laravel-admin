b24ac0af52f9e00239d0cd69687d1cadd92f5423
Victor LUCCHESE <vlucchese@connected-company.fr>
2018-03-26 13:42:40 +0200

    Add french translation for v1.4.*

---
+++ lang/fr/lang.php
@@ -0,0 +1,83 @@
+<?php
+
+return [
+    'online'                => 'En ligne',
+    'login'                 => 'Connexion',
+    'logout'                => 'Déconnexion',
+    'setting'               => 'Réglages',
+    'name'                  => 'Nom',
+    'username'              => 'Nom d\'utilisateur',
+    'password'              => 'Mot de passe',
+    'password_confirmation' => 'Confirmation  du mot de passe',
+    'remember_me'           => 'Se souvenir de moi',
+    'user_setting'          => 'Paramètre d\'utilisateur',
+    'avatar'                => 'Avatar',
+
+    'list'          => 'Liste',
+    'new'           => 'Nouveau',
+    'create'        => 'Créer',
+    'delete'        => 'Effacer',
+    'remove'        => 'Supprimer',
+    'edit'          => 'Éditer',
+    'view'          => 'Voir',
+    'browse'        => 'Parcourir',
+    'reset'         => 'Réinitialiser',
+    'export'        => 'Exportation',
+    'batch_delete'  => 'Suppression par lot',
+    'save'          => 'Sauvegarder',
+    'refresh'       => 'Rafraîchir',
+    'order'         => 'Commander',
+    'expand'        => 'Étendre',
+    'collapse'      => 'Replier',
+    'filter'        => 'Filtrer',
+    'close'         => 'Fermer',
+    'show'          => 'Montrer',
+    'entries'       => 'entrées',
+    'captcha'       => 'Captcha',
+
+    'action'           => 'Action',
+    'title'            => 'Titre',
+    'description'      => 'Description',
+    'back'             => 'Revenir',
+    'back_to_list'     => 'Revenir à la liste',
+    'submit'           => 'Soumettre',
+    'menu'             => 'Menu',
+    'input'            => 'Entré',
+    'succeeded'        => 'Réussi',
+    'failed'           => 'Échoué',
+    'delete_confirm'   => 'Êtes-vous sûr de vouloir supprimer cet élément ?',
+    'delete_succeeded' => 'Suppression réussi !',
+    'delete_failed'    => 'Suppression échouée !',
+    'update_succeeded' => 'Mise à jour réussie !',
+    'save_succeeded'   => 'Sauvegarde réussie !',
+    'refresh_succeeded'=> 'Rafraîchit !',
+    'login_successful' => 'Connexion réussie',
+
+    'choose'        => 'Sélectionner',
+    'choose_file'   => 'Choisir le fichier',
+    'choose_image'  => 'Choisir une image',
+
+    'more'          => 'Plus',
+    'deny'          => 'Permission refusée',
+
+    'administrator' => 'Administrateur',
+    'roles'         => 'Roles',
+    'permissions'   => 'Autorisations',
+    'slug'          => 'Slug',
+
+    'created_at'    => 'Créé le',
+    'updated_at'    => 'Mis à jour le',
+
+    'alert'         => 'Alerte',
+
+    'parent_id'     => 'Parent',
+    'icon'          => 'Icon',
+    'uri'           => 'URI',
+
+    'operation_log'       => 'Journal d\'opération',
+    'parent_select_error' => 'Erreur de sélection du parent',
+
+    'pagination'    => [
+        'range'  => 'Affichage de :first à :last sur :total',
+    ],
+];
