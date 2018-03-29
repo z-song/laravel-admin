9e598f46af51b32a79204d5d412e83aa8073ebb3
carlclone <906561974@qq.com>
2018-03-29 17:13:45 +0800

    让多条件搜索的情况下,某些条件可以重新选择为空值

--- views/filter/select.blade.php
+++ views/filter/select.blade.php
@@ -1,4 +1,5 @@
 <select class="form-control " style="width: 100%;" name="{{$name}}">
+        <option value=" ">Empty</option>
     @foreach($options as $select => $option)
         <option value="{{$select}}" {{ $select == request($name, $value) ?'selected':'' }}>{{$option}}</option>
     @endforeach
