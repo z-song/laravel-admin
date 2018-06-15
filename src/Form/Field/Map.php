decf4b29f0d14e99ea487d6a57b4ec18d73fa76a
Simon Peters <simon@spaced.be>
2018-06-15 11:05:40 +0200

    Fix bug when adding multiple maps to a form

--- src/Form/Field/Map.php
+++ src/Form/Field/Map.php
@@ -53,85 +53,89 @@ class Map extends Field
     public function useGoogleMap()
     {
         $this->script = <<<EOT
-        function initGoogleMap(name) {
-            var lat = $('#{$this->id['lat']}');
-            var lng = $('#{$this->id['lng']}');
-
-            var LatLng = new google.maps.LatLng(lat.val(), lng.val());
-
-            var options = {
-                zoom: 13,
-                center: LatLng,
-                panControl: false,
-                zoomControl: true,
-                scaleControl: true,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
+        (function() {
+            function initGoogleMap(name) {
+                var lat = $('#{$this->id['lat']}');
+                var lng = $('#{$this->id['lng']}');
+    
+                var LatLng = new google.maps.LatLng(lat.val(), lng.val());
+    
+                var options = {
+                    zoom: 13,
+                    center: LatLng,
+                    panControl: false,
+                    zoomControl: true,
+                    scaleControl: true,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
+    
+                var container = document.getElementById("map_"+name);
+                var map = new google.maps.Map(container, options);
+    
+                var marker = new google.maps.Marker({
+                    position: LatLng,
+                    map: map,
+                    title: 'Drag Me!',
+                    draggable: true
+                });
+    
+                google.maps.event.addListener(marker, 'dragend', function (event) {
+                    lat.val(event.latLng.lat());
+                    lng.val(event.latLng.lng());
+                });
             }
-
-            var container = document.getElementById("map_"+name);
-            var map = new google.maps.Map(container, options);
-
-            var marker = new google.maps.Marker({
-                position: LatLng,
-                map: map,
-                title: 'Drag Me!',
-                draggable: true
-            });
-
-            google.maps.event.addListener(marker, 'dragend', function (event) {
-                lat.val(event.latLng.lat());
-                lng.val(event.latLng.lng());
-            });
-        }
-
-        initGoogleMap('{$this->id['lat']}{$this->id['lng']}');
+    
+            initGoogleMap('{$this->id['lat']}{$this->id['lng']}');
+        })();
 EOT;
     }
 
     public function useTencentMap()
     {
         $this->script = <<<EOT
-        function initTencentMap(name) {
-            var lat = $('#{$this->id['lat']}');
-            var lng = $('#{$this->id['lng']}');
-
-            var center = new qq.maps.LatLng(lat.val(), lng.val());
-
-            var container = document.getElementById("map_"+name);
-            var map = new qq.maps.Map(container, {
-                center: center,
-                zoom: 13
-            });
-
-            var marker = new qq.maps.Marker({
-                position: center,
-                draggable: true,
-                map: map
-            });
-
-            if( ! lat.val() || ! lng.val()) {
-                var citylocation = new qq.maps.CityService({
-                    complete : function(result){
-                        map.setCenter(result.detail.latLng);
-                        marker.setPosition(result.detail.latLng);
-                    }
+        (function() {
+            function initTencentMap(name) {
+                var lat = $('#{$this->id['lat']}');
+                var lng = $('#{$this->id['lng']}');
+    
+                var center = new qq.maps.LatLng(lat.val(), lng.val());
+    
+                var container = document.getElementById("map_"+name);
+                var map = new qq.maps.Map(container, {
+                    center: center,
+                    zoom: 13
                 });
-
-                citylocation.searchLocalCity();
+    
+                var marker = new qq.maps.Marker({
+                    position: center,
+                    draggable: true,
+                    map: map
+                });
+    
+                if( ! lat.val() || ! lng.val()) {
+                    var citylocation = new qq.maps.CityService({
+                        complete : function(result){
+                            map.setCenter(result.detail.latLng);
+                            marker.setPosition(result.detail.latLng);
+                        }
+                    });
+    
+                    citylocation.searchLocalCity();
+                }
+    
+                qq.maps.event.addListener(map, 'click', function(event) {
+                    marker.setPosition(event.latLng);
+                });
+    
+                qq.maps.event.addListener(marker, 'position_changed', function(event) {
+                    var position = marker.getPosition();
+                    lat.val(position.getLat());
+                    lng.val(position.getLng());
+                });
             }
-
-            qq.maps.event.addListener(map, 'click', function(event) {
-                marker.setPosition(event.latLng);
-            });
-
-            qq.maps.event.addListener(marker, 'position_changed', function(event) {
-                var position = marker.getPosition();
-                lat.val(position.getLat());
-                lng.val(position.getLng());
-            });
-        }
-
-        initTencentMap('{$this->id['lat']}{$this->id['lng']}');
+    
+            initTencentMap('{$this->id['lat']}{$this->id['lng']}');
+        })();
 EOT;
     }
 }
