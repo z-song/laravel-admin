609b2fc178214b5907914f433af6dfb2d9e53f40
jordan <newlt@mail.com>
2018-06-22 03:16:01 +0800

    admin protect

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -122,4 +122,50 @@ class UserController extends Controller
             });
         });
     }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id)
+    {
+        if ($id == 1 && Admin::user()->id != 1) {
+            return back();
+        }
+
+        return $this->form()->update($id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $ids = explode(',', $id);
+        if (in_array(1, $ids)) {
+            return response()->json([
+                'status'  => false,
+                'message' => trans('admin.delete_failed'),
+            ]);
+        }
+
+        if ($this->form()->destroy($id)) {
+            return response()->json([
+                'status'  => true,
+                'message' => trans('admin.delete_succeeded'),
+            ]);
+        } else {
+            return response()->json([
+                'status'  => false,
+                'message' => trans('admin.delete_failed'),
+            ]);
+        }
+    }
 }
