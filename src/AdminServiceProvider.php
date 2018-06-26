9f2d74e65fc66fe28ac936ad31d1520c1a42afd4
Dynamo Huang <techchongqing@gmail.com>
2018-06-26 16:53:51 +0800

    Remove default feature of double encoding enable in laravel 5.6 or later.
    Remove default feature of double encoding enable in laravel 5.6 or later.
    For  fix  this issue https://github.com/z-song/laravel-admin/issues/1844

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Encore\Admin;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AdminServiceProvider extends ServiceProvider
 {
@@ -64,6 +65,12 @@ class AdminServiceProvider extends ServiceProvider
 //            $this->publishes([__DIR__.'/../resources/views' => resource_path('views/admin')],           'laravel-admin-views');
             $this->publishes([__DIR__.'/../database/migrations' => database_path('migrations')], 'laravel-admin-migrations');
             $this->publishes([__DIR__.'/../resources/assets' => public_path('vendor/laravel-admin')], 'laravel-admin-assets');
+        }
+        
+        //remove default feature of double encoding enable in laravel 5.6 or later.
+        $bladeReflectionClass = new \ReflectionClass('\Illuminate\View\Compilers\BladeCompiler');
+        if($bladeReflectionClass->hasMethod('withoutDoubleEncoding')){
+            Blade::withoutDoubleEncoding();
         }
     }
 
