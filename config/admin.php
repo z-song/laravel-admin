cc335736ff25fbd8206cb63c4647aa4d98a777f7
Encore <zosong@126.com>
2018-08-04 16:50:23 +0800

    [docs] Update docs for `config/admin.php`

--- config/admin.php
+++ config/admin.php
@@ -3,23 +3,49 @@
 return [
 
     /*
-     * Laravel-admin name.
-     */
+    |--------------------------------------------------------------------------
+    | Laravel-admin name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of laravel-admin, This setting is displayed on the
+    | login page.
+    |
+    */
     'name' => 'Laravel-admin',
 
     /*
-     * Logo in admin panel header.
-     */
+    |--------------------------------------------------------------------------
+    | Laravel-admin logo
+    |--------------------------------------------------------------------------
+    |
+    | The logo of all admin pages. You can also set it as an image by using a
+    | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
+    |
+    */
     'logo' => '<b>Laravel</b> admin',
 
     /*
-     * Mini-logo in admin panel header.
-     */
+    |--------------------------------------------------------------------------
+    | Laravel-admin mini logo
+    |--------------------------------------------------------------------------
+    |
+    | The logo of all admin pages when the sidebar menu is collapsed. You can
+    | also set it as an image by using a `img` tag, eg
+    | '<img src="http://logo-url" alt="Admin logo">'.
+    |
+    */
     'logo-mini' => '<b>La</b>',
 
     /*
-     * Route configuration.
-     */
+    |--------------------------------------------------------------------------
+    | Laravel-admin route settings
+    |--------------------------------------------------------------------------
+    |
+    | The routing configuration of the admin page, including the path prefix,
+    | the controller namespace, and the default middleware. If you want to
+    | access through the root path, just set the prefix to empty string.
+    |
+    */
     'route' => [
 
         'prefix' => 'admin',
@@ -30,23 +56,46 @@ return [
     ],
 
     /*
-     * Laravel-admin install directory.
-     */
+    |--------------------------------------------------------------------------
+    | Laravel-admin install directory
+    |--------------------------------------------------------------------------
+    |
+    | The installation directory of the controller and routing configuration
+    | files of the administration page. The default is `app/Admin`, which must
+    | be set before running `artisan admin::install` to take effect.
+    |
+    */
     'directory' => app_path('Admin'),
 
     /*
-     * Laravel-admin html title.
-     */
+    |--------------------------------------------------------------------------
+    | Laravel-admin html title
+    |--------------------------------------------------------------------------
+    |
+    | Html title for all pages.
+    |
+    */
     'title' => 'Admin',
 
     /*
-     * Use `https`.
-     */
+    |--------------------------------------------------------------------------
+    | Access via `https`
+    |--------------------------------------------------------------------------
+    |
+    | If your page is going to be accessed via https, set it to `true`.
+    |
+    */
     'secure' => false,
 
     /*
-     * Laravel-admin auth setting.
-     */
+    |--------------------------------------------------------------------------
+    | Laravel-admin auth setting
+    |--------------------------------------------------------------------------
+    |
+    | Authentication settings for all admin pages. Include an authentication
+    | guard and a user provider setting of authentication driver.
+    |
+    */
     'auth' => [
         'guards' => [
             'admin' => [
@@ -64,12 +113,20 @@ return [
     ],
 
     /*
-     * Laravel-admin upload setting.
-     */
+    |--------------------------------------------------------------------------
+    | Laravel-admin upload setting
+    |--------------------------------------------------------------------------
+    |
+    | File system configuration for form upload files and images, including
+    | disk and upload path.
+    |
+    */
     'upload' => [
 
+        // Disk in `config/filesystem.php`.
         'disk' => 'admin',
 
+        // Image and file upload path under the disk above.
         'directory' => [
             'image' => 'images',
             'file'  => 'files',
@@ -77,8 +134,13 @@ return [
     ],
 
     /*
-     * Laravel-admin database setting.
-     */
+    |--------------------------------------------------------------------------
+    | Laravel-admin database settings
+    |--------------------------------------------------------------------------
+    |
+    | Here are database settings for laravel-admin builtin model & tables.
+    |
+    */
     'database' => [
 
         // Database connection for following tables.
@@ -109,8 +171,13 @@ return [
     ],
 
     /*
-     * By setting this option to open or close operation log in laravel-admin.
-     */
+    |--------------------------------------------------------------------------
+    | User operation log setting
+    |--------------------------------------------------------------------------
+    |
+    | By setting this option to open or close operation log in laravel-admin.
+    |
+    */
     'operation_log' => [
 
         'enable' => true,
@@ -119,7 +186,7 @@ return [
          * Routes that will not log to database.
          *
          * All method to path like: admin/auth/logs
-         * or specific method to path like: get:admin/auth/logs
+         * or specific method to path like: get:admin/auth/logs.
          */
         'except' => [
             'admin/auth/logs*',
@@ -127,34 +194,64 @@ return [
     ],
 
     /*
-     * @see https://adminlte.io/docs/2.4/layout
-     */
+    |--------------------------------------------------------------------------
+    | Application Skin
+    |--------------------------------------------------------------------------
+    |
+    | This value is the skin of admin pages.
+    | @see https://adminlte.io/docs/2.4/skin
+    |
+    | Supported:
+    |    "skin-blue", "skin-blue-light", "skin-yellow", "skin-yellow-light",
+    |    "skin-green", "skin-green-light", "skin-purple", "skin-purple-light",
+    |    "skin-red", "skin-red-light", "skin-black", "skin-black-light".
+    |
+    */
     'skin' => 'skin-blue-light',
 
     /*
-    |---------------------------------------------------------|
-    |LAYOUT OPTIONS | fixed                                   |
-    |               | layout-boxed                            |
-    |               | layout-top-nav                          |
-    |               | sidebar-collapse                        |
-    |               | sidebar-mini                            |
-    |---------------------------------------------------------|
-     */
+    |--------------------------------------------------------------------------
+    | Application layout
+    |--------------------------------------------------------------------------
+    |
+    | This value is the layout of admin pages.
+    | @see https://adminlte.io/docs/2.4/layout
+    |
+    | Supported: "fixed", "layout-boxed", "layout-top-nav", "sidebar-collapse",
+    | "sidebar-mini".
+    |
+    */
     'layout' => ['sidebar-mini', 'sidebar-collapse'],
 
     /*
-     * Background image in login page
-     */
+    |--------------------------------------------------------------------------
+    | Login page background image
+    |--------------------------------------------------------------------------
+    |
+    | This value is used to set the background image of login page.
+    |
+    */
     'login_background_image' => '',
 
     /*
-     * Version displayed in footer.
-     */
+    |--------------------------------------------------------------------------
+    | Version
+    |--------------------------------------------------------------------------
+    |
+    | This version number set will appear in the page footer.
+    |
+    */
     'version' => '1.5.x-dev',
 
     /*
-     * Settings for extensions.
-     */
+    |--------------------------------------------------------------------------
+    | Settings for extensions.
+    |--------------------------------------------------------------------------
+    |
+    | You can find all available extensions here
+    | https://github.com/laravel-admin-extensions.
+    |
+    */
     'extensions' => [
 
     ],
