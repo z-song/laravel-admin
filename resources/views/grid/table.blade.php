5b3582e77b9f191a8afbd62ba50fb2371171f850
Hsatou Exceedone <hsatou@exceedone.co.jp>
2018-08-14 14:56:43 +0900

    add thead and tbody in views/grid/table.blade.php

--- resources/views/grid/table.blade.php
+++ resources/views/grid/table.blade.php
@@ -20,21 +20,25 @@
     <!-- /.box-header -->
     <div class="box-body table-responsive no-padding">
         <table class="table table-hover">
-            <tr>
-                @foreach($grid->columns() as $column)
-                <th>{{$column->getLabel()}}{!! $column->sorter() !!}</th>
-                @endforeach
-            </tr>
+            <thead>
+                <tr>
+                    @foreach($grid->columns() as $column)
+                    <th>{{$column->getLabel()}}{!! $column->sorter() !!}</th>
+                    @endforeach
+                </tr>
+            </thead>
 
-            @foreach($grid->rows() as $row)
-            <tr {!! $row->getRowAttributes() !!}>
-                @foreach($grid->columnNames as $name)
-                <td {!! $row->getColumnAttributes($name) !!}>
-                    {!! $row->column($name) !!}
-                </td>
+            <tbody>
+                @foreach($grid->rows() as $row)
+                <tr {!! $row->getRowAttributes() !!}>
+                    @foreach($grid->columnNames as $name)
+                    <td {!! $row->getColumnAttributes($name) !!}>
+                        {!! $row->column($name) !!}
+                    </td>
+                    @endforeach
+                </tr>
                 @endforeach
-            </tr>
-            @endforeach
+            </tbody>
 
             {!! $grid->renderFooter() !!}
 
