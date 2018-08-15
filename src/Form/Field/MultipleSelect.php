4277c2cb9ea140861e7c0a5c2f676ad275a1f5e0
Hsatou Exceedone <hsatou@exceedone.co.jp>
2018-08-15 16:53:46 +0900

    Bug fix MultipleSelect.

--- src/Form/Field/MultipleSelect.php
+++ src/Form/Field/MultipleSelect.php
@@ -48,7 +48,10 @@ class MultipleSelect extends Select
         }
 
         if (is_array($relations)) {
-            if (is_string(current($relations))) {
+            if(is_null(current($relations))){
+                $this->value = null;
+            }
+            else if (is_string(current($relations))) {
                 $this->value = $relations;
             } else {
                 foreach ($relations as $relation) {
@@ -67,7 +70,10 @@ class MultipleSelect extends Select
         }
 
         if (is_array($relations)) {
-            if (is_string(current($relations))) {
+            if(is_null(current($relations))){
+                $this->original = null;
+            }
+            else if (is_string(current($relations))) {
                 $this->original = $relations;
             } else {
                 foreach ($relations as $relation) {
