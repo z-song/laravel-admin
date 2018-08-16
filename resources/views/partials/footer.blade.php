f50794e30ca7685962835b5379ea8e8280d00fb4
jxlwqq <jxlwqq@gmail.com>
2018-08-16 15:58:37 +0800

    Add page footer show config

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,11 @@
-<!-- Main Footer -->
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
-    </div>
-    <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
-</footer>
+@if(config('admin.page_footer_show', true))
+    <!-- Main Footer -->
+    <footer class="main-footer">
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+            <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
+        </div>
+        <!-- Default to the left -->
+        <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    </footer>
+@endif
