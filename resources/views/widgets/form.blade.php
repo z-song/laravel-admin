132a0992fe750b4f59fca78bd566fbf38cf2ed7e
Hsatou Exceedone <hsatou@exceedone.co.jp>
2018-08-17 14:17:34 +0900

    Add disableViewCheck and disableEditingCheck, etc...

--- resources/views/widgets/form.blade.php
+++ resources/views/widgets/form.blade.php
@@ -7,22 +7,28 @@
 
     </div>
 
-    <!-- /.box-body -->
-    <div class="box-footer">
     @if ($method != 'GET')
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
     @endif
+    
+    <!-- /.box-body -->
+    @if(count($buttons) > 0)
+    <div class="box-footer">
         <div class="col-md-2"></div>
 
         <div class="col-md-8">
+            @if(in_array('reset', $buttons))
             <div class="btn-group pull-left">
                 <button type="reset" class="btn btn-warning pull-right">{{ trans('admin.reset') }}</button>
             </div>
+            @endif
+
+            @if(in_array('submit', $buttons))
             <div class="btn-group pull-right">
                 <button type="submit" class="btn btn-info pull-right">{{ trans('admin.submit') }}</button>
             </div>
-
+            @endif
         </div>
-
     </div>
+    @endif
 </form>
