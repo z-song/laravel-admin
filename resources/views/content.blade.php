ffa3060f30549ba3835383111595daaa1bade9f9
z-song <zosong@126.com>
2018-08-29 22:59:24 +0800

    [fix] Fix issue #2401

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <section class="content-header">
         <h1>
-            {{ $header or trans('admin.title') }}
-            <small>{{ $description or trans('admin.description') }}</small>
+            {{ $header ?: trans('admin.title') }}
+            <small>{{ $description ?: trans('admin.description') }}</small>
         </h1>
 
         <!-- breadcrumb start -->
