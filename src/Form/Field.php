61cd61c7383c939e06d3e793026716ea8c0f2a3b
jxlwqq <jxlwqq@gmail.com>
2018-09-07 14:56:53 +0800

    Add pattern method
    Specifies a regular expression against which to validate the value of the input.

--- src/Form/Field.php
+++ src/Form/Field.php
@@ -715,7 +715,19 @@ class Field implements Renderable
 
         return $this;
     }
-
+    
+    
+    /**
+     * Specifies a regular expression against which to validate the value of the input.
+     * 
+     * @param string $regexp
+     * 
+     * @return Field
+     */
+    public function pattern($regexp)
+    {
+        return $this->attribute('pattern', $regexp);
+    }
 
     /**
      * Set the field automatically get focus.
