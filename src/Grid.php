5d6aa672f918df80d012fd8fafb5dd7b191146d4
z-song <zosong@126.com>
2018-09-20 13:21:42 +0800

    [fix]Fix issue #2529

--- src/Grid.php
+++ src/Grid.php
@@ -211,6 +211,8 @@ class Grid
             return;
         }
 
+        ob_end_clean();
+
         $this->model()->usePaginate(false);
 
         $exporter = (new Exporter($this))->resolve($this->exporter)->withScope($scope);
