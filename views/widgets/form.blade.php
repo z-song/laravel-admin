b55d7158cf1620b970a4c1a801b7472efbd7c32c
Andrew Busel <spinyman@tut.by>
2018-09-20 16:07:58 +0300

    box-footer row fix

--- views/widgets/form.blade.php
+++ views/widgets/form.blade.php
@@ -8,20 +8,22 @@
     </div>
 
     <!-- /.box-body -->
-    <div class="box-footer row">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="col-sm-2">
-        </div>
-        <div class="col-sm-2">
-            <div class="btn-group pull-left">
-                <button type="reset" class="btn btn-warning pull-right">{{ trans('admin::lang.reset') }}</button>
+    <div class="box-footer">
+        <div class="row">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="col-sm-2">
             </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="btn-group pull-right">
-                <button type="submit" class="btn btn-info pull-right">{{ trans('admin::lang.submit') }}</button>
+            <div class="col-sm-2">
+                <div class="btn-group pull-left">
+                    <button type="reset" class="btn btn-warning pull-right">{{ trans('admin::lang.reset') }}</button>
+                </div>
             </div>
-        </div>
+            <div class="col-sm-4">
+                <div class="btn-group pull-right">
+                    <button type="submit" class="btn btn-info pull-right">{{ trans('admin::lang.submit') }}</button>
+                </div>
+            </div>
 
+        </div>
     </div>
 </form>
