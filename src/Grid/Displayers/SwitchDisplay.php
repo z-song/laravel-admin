357075740e5e1fc6f1ea53042790aed80cc3ef5f
zanemmm <pi@0php.net>
2018-09-21 18:25:18 +0800

    fix #2546 修复关联表不支持 switch 的 bug

--- src/Grid/Displayers/SwitchDisplay.php
+++ src/Grid/Displayers/SwitchDisplay.php
@@ -22,7 +22,7 @@ class SwitchDisplay extends AbstractDisplayer
     {
         $this->updateStates($states);
 
-        $name = $this->column->getName();
+        $name = str_replace('.', '_', $this->column->getName());
 
         $class = "grid-switch-{$name}";
 
