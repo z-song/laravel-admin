ba8f5d59bf05b8eee51d983eb702c43473b70d1c
dobs <webmaster.dobs@gmail.com>
2018-09-28 22:27:27 +0300

    Styleci fix

--- src/Form.php
+++ src/Form.php
@@ -1261,7 +1261,7 @@ class Form implements Renderable
         if ($slice != 0) {
             $segments = array_slice($segments, 0, $slice);
         }
-        
+
         return implode('/', $segments);
     }
 
