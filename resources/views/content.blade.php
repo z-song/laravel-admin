18bc92dd787cc1bae4ac453b9f5b98c3365890f8
z-song <zosong@126.com>
2018-09-29 23:53:51 +0800

    [fix] Add a config to control default breadcrumb

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -31,14 +31,14 @@
                 @endif
             @endforeach
         </ol>
-        @else
+        @elseif(config('admin.enable_default_breadcrumb'))
         <ol class="breadcrumb" style="margin-right: 30px;">
             <li><a href="{{ admin_url('/') }}"><i class="fa fa-dashboard"></i> Home</a></li>   
-        @for($i = 2; $i <= count(Request::segments()); $i++)
-            <li>
-            {{ucfirst(Request::segment($i))}}
-            </li>
-        @endfor
+            @for($i = 2; $i <= count(Request::segments()); $i++)
+                <li>
+                {{ucfirst(Request::segment($i))}}
+                </li>
+            @endfor
         </ol>
         @endif
 
