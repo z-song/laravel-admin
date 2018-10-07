5139434bda4bd28776195e30f677939eee5b6118
Andrew Busel <spinyman@tut.by>
2018-10-07 17:13:49 +0300

    1) added possibility to use many captchas on the page. 2) added attributes to captcha image regarding its config (config/captcha.php)

--- views/form/captcha.blade.php
+++ views/form/captcha.blade.php
@@ -1,5 +1,5 @@
-<div class="form-group row {!! !$errors->has($errorKey) ?: 'has-error' !!}">
-
+@php($skin = config('captcha.skin', 'default'))
+<div class="row form-group {!! !$errors->has($errorKey) ?: 'has-error' !!}">
     <label for="{{$id}}" class="col-sm-{{$width['label']}} control-label">{{$label}}</label>
 
     <div class="col-sm-{{$width['field']}}">
@@ -7,11 +7,11 @@
         @include('admin::form.error')
 
         <div class="input-group" style="width: 250px;">
+            <input {!! $attributes !!} autocomplete="off" />
 
-            <input {!! $attributes !!} />
-
-            <span class="input-group-addon clearfix" style="padding: 1px;"><img id="{{$column}}-captcha" src="{{ captcha_src() }}" style="height:30px;cursor: pointer;"  title="Click to refresh"/></span>
-
+            <span class="input-group-addon clearfix" style="padding: 1px;">
+                <img id="{{$column}}-captcha" src="{{ captcha_src($skin) }}" class="captcha-image" style="height:{{config("captcha.{$skin}.height", 30)}}px;width:{{config("captcha.{$skin}.width", 150)}}px;cursor: pointer;"  title="Click to refresh"/>
+            </span>
         </div>
 
         @include('admin::form.help-block')
