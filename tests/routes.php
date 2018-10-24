3d3f7a4f9cab47eb246c01bdcbc0577f7fb8b393
david <1033404553@qq.com>
2018-10-24 12:58:12 +0800

    fix_route_style

--- tests/routes.php
+++ tests/routes.php
@@ -9,10 +9,10 @@ Route::group([
     $router->resource('multiple-images', MultipleImageController::class);
     $router->resource('files', FileController::class);
     $router->resource('users', UserController::class);
-    $router->get('persons/{id}/edit', PersonController::class . '@edit');
+    $router->get('persons/{id}/edit', PersonController::class.'@edit');
 
     $router->prefix('api')->namespace('Api')->group(function ($router) {
-        $router->get('countries', CountryController::class . '@index');
-        $router->get('cities', CountryController::class . '@getCities');
+        $router->get('countries', CountryController::class.'@index');
+        $router->get('cities', CountryController::class.'@getCities');
     });
 });
