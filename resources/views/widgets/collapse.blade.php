28db060e672857d65cdc91a6fd9785e57ba9c5e2
Anwar Hussen <38097917+anwar0x09@users.noreply.github.com>
2018-10-29 06:39:38 +0600

    multiple collapse in single page problem solved
    updated div id with collapse title for multiple collapse support in single page

--- resources/views/widgets/collapse.blade.php
+++ resources/views/widgets/collapse.blade.php
@@ -3,12 +3,12 @@
     <div class="panel box box-primary" style="margin-bottom: 0px">
         <div class="box-header with-border">
             <h4 class="box-title">
-                <a data-toggle="collapse" data-parent="#{{$id}}" href="#collapse{{ $key }}">
+                <a data-toggle="collapse" data-parent="#{{$id}}" href="#collapse_{{snake_case($item['title'])}}_{{ $key }}">
                     {{ $item['title'] }}
                 </a>
             </h4>
         </div>
-        <div id="collapse{{ $key }}" class="panel-collapse collapse {{ $key == 0 ? 'in' : '' }}">
+        <div id="collapse_{{snake_case($item['title'])}}_{{ $key }}" class="panel-collapse collapse {{ $key == 0 ? 'in' : '' }}">
             <div class="box-body">
                 {!! $item['content'] !!}
             </div>
