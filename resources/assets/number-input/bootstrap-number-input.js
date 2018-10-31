644a49b1d12558b726d888cf527e5b755a967553
Kevin Iuretig <yurik94@gmail.com>
2018-10-31 02:42:05 +0100

    Allow float numbers with bootstrap-number-input
    Credits: https://github.com/wpic/bootstrap-number-input/issues/6

--- resources/assets/number-input/bootstrap-number-input.js
+++ resources/assets/number-input/bootstrap-number-input.js
@@ -19,51 +19,63 @@
  * ========================================================================
  */
 
-(function ($) {
+(function ( $ ) {
 
-    $.fn.bootstrapNumber = function (options) {
+    $.fn.bootstrapNumber = function( options ) {
 
         var settings = $.extend({
             upClass: 'default',
             downClass: 'default',
+            upText: '+',
+            downText: '-',
             center: true
-        }, options);
+            }, options );
 
-        return this.each(function (e) {
+        return this.each(function(e) {
             var self = $(this);
             var clone = self.clone();
 
             var min = self.attr('min');
             var max = self.attr('max');
+            var step = parseFloat(self.attr('step')) || 1;
+            var dec = decimalPlaces(step);
 
             function setText(n) {
-                if ((min && n < min) || (max && n > max)) {
+                if (isNaN(n) || (min && n < min) || (max && n > max)) {
                     return false;
                 }
 
+                if($.isNumeric(n)){  // it is a number
+                    n = +n.toFixed(dec);
+                } else{
+                   // not a number
+                   return false;
+                }
+
                 clone.focus().val(n);
+                clone.trigger('change');
                 return true;
             }
 
             var group = $("<div class='input-group'></div>");
-            var down = $("<button type='button'>-</button>").attr('class', 'btn btn-' + settings.downClass).click(function () {
-                setText(parseInt(clone.val()) - 1);
+            var down = $("<button type='button'>" + settings.downText + "</button>").attr('class', 'btn btn-' + settings.downClass).click(function() {
+                setText(parseFloat((clone.val() || clone.attr('min'))) - step);
             });
-            var up = $("<button type='button'>+</button>").attr('class', 'btn btn-' + settings.upClass).click(function () {
-                setText(parseInt(clone.val()) + 1);
+            var up = $("<button type='button'>" + settings.upText + "</button>").attr('class', 'btn btn-' + settings.upClass).click(function() {
+                setText(parseFloat((clone.val() || clone.attr('min'))) + step);
             });
             $("<span class='input-group-btn'></span>").append(down).appendTo(group);
             clone.appendTo(group);
-            if (clone) {
+            if(clone && settings.center) {
                 clone.css('text-align', 'center');
             }
             $("<span class='input-group-btn'></span>").append(up).appendTo(group);
 
             // remove spins from original
-            clone.prop('type', 'text').keydown(function (e) {
+            clone.prop('type', 'text').keydown(function(e) {
                 if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-					(e.keyCode == 65 && e.ctrlKey === true) ||
-					(e.keyCode >= 35 && e.keyCode <= 39)) {
+                    (e.keyCode == 65 && e.ctrlKey === true) ||
+                    (e.keyCode >= 35 && e.keyCode <= 39)) {
                     return;
                 }
                 if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
@@ -73,24 +85,23 @@
                 var c = String.fromCharCode(e.which);
                 var n = parseInt(clone.val() + c);
 
-                //if ((min && n < min) || (max && n > max)) {
-                //    e.preventDefault();
-                //}
-            });
-
-            clone.prop('type', 'text').blur(function (e) {
-                var c = String.fromCharCode(e.which);
-                var n = parseInt(clone.val() + c);
-                if ((min && n < min)) {
-                    setText(min);
-                }
-                else if (max && n > max) {
-                    setText(max);
+                if ((min && n < min) || (max && n > max)) {
+                    e.preventDefault();
                 }
             });
-
 
             self.replaceWith(group);
         });
     };
-}(jQuery));
+
+    decimalPlaces = function(num) {
+      var match = (''+num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+      if (!match) { return 0; }
+      return Math.max(
+           0,
+           // Number of digits right of decimal point.
+           (match[1] ? match[1].length : 0)
+           // Adjust for scientific notation.
+           - (match[2] ? +match[2] : 0));
+    };
+} ( jQuery ));
