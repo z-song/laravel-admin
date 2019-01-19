23d85fd78db466ed5a6e80d2665a0e255add041b
dobs <webmaster.dobs@gmail.com>
2019-01-19 15:42:44 +0200

    Add custom fa-icon

--- src/Form/Field/Text.php
+++ src/Form/Field/Text.php
@@ -9,6 +9,25 @@ class Text extends Field
     use PlainInput;
 
     /**
+     * @var string
+     */
+    protected $icon = 'fa-pencil';
+
+    /**
+     * Add custom fa-icon.
+     *
+     * @param string $icon
+     *
+     * @return $this
+     */
+    public function addIcon($icon)
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+
+    /**
      * Render this filed.
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
@@ -17,7 +36,7 @@ class Text extends Field
     {
         $this->initPlainInput();
 
-        $this->prepend('<i class="fa fa-pencil fa-fw"></i>')
+        $this->prepend('<i class="fa ' . $this->icon . ' fa-fw"></i>')
             ->defaultAttribute('type', 'text')
             ->defaultAttribute('id', $this->id)
             ->defaultAttribute('name', $this->elementName ?: $this->formatName($this->column))
