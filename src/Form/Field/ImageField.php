e47baf0ad292c299d697319dd498cffc6590fdd7
Hanson <h@hanc.cc>
2019-01-24 18:44:03 +0800

    修复 image 类型使用 macro 失败

--- src/Form/Field/ImageField.php
+++ src/Form/Field/ImageField.php
@@ -58,6 +58,10 @@ trait ImageField
      */
     public function __call($method, $arguments)
     {
+        if (static::hasMacro($method)) {
+            return $this;
+        }
+        
         if (!class_exists(ImageManagerStatic::class)) {
             throw new \Exception('To use image handling and manipulation, please install [intervention/image] first.');
         }
