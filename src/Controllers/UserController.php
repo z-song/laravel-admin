634a1a63f494450ebd194b3b3f8ed4c638966ad8
z-song <zosong@126.com>
2019-03-07 12:18:33 +0800

    Fix issue #3030

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -145,9 +145,14 @@ class UserController extends Controller
 
         $form->display('id', 'ID');
 
-        $userTable = config('admin.database.users_table');
+        if (request()->isMethod('POST')) {
+            $userTable = config('admin.database.users_table');
+            $userNameRules = "required|unique:{$userTable}";
+        } else {
+            $userNameRules = 'required';
+        }
 
-        $form->text('username', trans('admin.username'))->rules("required|unique:{$userTable}");
+        $form->text('username', trans('admin.username'))->rules($userNameRules);
         $form->text('name', trans('admin.name'))->rules('required');
         $form->image('avatar', trans('admin.avatar'));
         $form->password('password', trans('admin.password'))->rules('required|confirmed');
