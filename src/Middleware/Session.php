f85c01e1e5208d9c5bfd86ea78ace2ca77bf5440
z-song <zosong@126.com>
2019-03-07 11:15:51 +0800

    Fix issue #3051

---
+++ src/Middleware/Session.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Encore\Admin\Middleware;
+
+use Illuminate\Http\Request;
+
+class Session
+{
+    public function handle(Request $request, \Closure $next)
+    {
+        $path = '/' . trim(config('admin.route.prefix'), '/');
+
+        config(['session.path' => $path]);
+
+        if ($domain = config('admin.route.domain')) {
+            config(['session.domain' => $domain]);
+        }
+
+        return $next($request);
+    }
+}
