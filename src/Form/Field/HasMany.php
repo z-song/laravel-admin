606294d4f96a48231d8b3b642f10d4557a8c7d1a
Robin Hoo <RobinHoo1973@users.noreply.github.com>
2019-03-11 15:23:57 +0800

    style ci

--- src/Form/Field/HasMany.php
+++ src/Form/Field/HasMany.php
@@ -9,7 +9,6 @@ use Encore\Admin\Form\NestedForm;
 use Illuminate\Database\Eloquent\Relations\HasMany as Relation;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
 
 if (!function_exists('Encore\Admin\Form\Field\array_key_attach_str')) {
     function array_key_attach_str(array $a, string $b, string $c = '.')
@@ -20,14 +19,14 @@ if (!function_exists('Encore\Admin\Form\Field\array_key_attach_str')) {
                 return ["{$b}{$c}{$u}" => $v];
             }, array_keys($a), array_values($a))
         );
-    };
+    }
 }
 
 if (!function_exists('Encore\Admin\Form\Field\array_clean_merge')) {
     function array_clean_merge(array $a, $b)
     {
         return $b ? array_merge($a, call_user_func_array('array_merge', $b)) : $a;
-    };
+    }
 }
 
 if (!function_exists('Encore\Admin\Form\Field\array_key_clean_undot')) {
@@ -42,7 +41,7 @@ if (!function_exists('Encore\Admin\Form\Field\array_key_clean_undot')) {
         }
 
         return $a;
-    };
+    }
 }
 
 if (!function_exists('Encore\Admin\Form\Field\array_key_clean')) {
@@ -53,7 +52,7 @@ if (!function_exists('Encore\Admin\Form\Field\array_key_clean')) {
         }, array_keys($a), array_values($a))) : $a;
 
         return $a;
-    };
+    }
 }
 
 /**
@@ -260,7 +259,7 @@ class HasMany extends Field
                         return call_user_func_array('array_merge', array_map(function ($u) use ($v, $field) {
                             $w = $field->label();
                             //Fix ResetInput Function! A Headache Implementation!
-                            $w .= is_array($field->column()) ? '[' . explode(':', explode('.', $v)[2])[0] . ']' : '';
+                            $w .= is_array($field->column()) ? '['.explode(':', explode('.', $v)[2])[0].']' : '';
 
                             return ["{$v}:{$u}" => $w];
                         }, array_keys($availInput[$k][$col])));
@@ -268,7 +267,7 @@ class HasMany extends Field
 
                     $w = $field->label();
                     //Fix ResetInput Function! A Headache Implementation!
-                    $w .= is_array($field->column()) ? '[' . explode(':', explode('.', $v)[2])[0] . ']' : '';
+                    $w .= is_array($field->column()) ? '['.explode(':', explode('.', $v)[2])[0].']' : '';
 
                     return [$v => $w];
                 }, $newColumn);
@@ -308,7 +307,6 @@ class HasMany extends Field
 
         return Validator::make($input, $rules, $messages, $attributes);
     }
-
 
     /**
      * Prepare input data for insert or update.
@@ -458,7 +456,7 @@ class HasMany extends Field
      */
     protected function setupScript($script)
     {
-        $method = 'setupScriptFor' . ucfirst($this->viewMode) . 'View';
+        $method = 'setupScriptFor'.ucfirst($this->viewMode).'View';
 
         call_user_func([$this, $method], $script);
     }
@@ -697,7 +695,7 @@ EOT;
         }, '');
 
         /* Build cell with hidden elements */
-        $template .= '<td class="hidden">' . implode('', $hidden) . '</td>';
+        $template .= '<td class="hidden">'.implode('', $hidden).'</td>';
 
         $this->setupScript(implode("\r\n", $scripts));
 
