222f6316d09390580cb4d004a3a154ff5a78cd46
Robin Hoo <RobinHoo1973@users.noreply.github.com>
2019-03-12 16:51:57 +0800

    initial commit

--- resources/views/form/error.blade.php
+++ resources/views/form/error.blade.php
@@ -1,10 +1,12 @@
 @if(is_array($errorKey))
-    @foreach($errorKey as $key => $col)
-        @if($errors->has($col.$key))
-            @foreach($errors->get($col.$key) as $message)
-                <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> {{$message}}</label><br/>
-            @endforeach
-        @endif
+    @foreach(array_unique(call_user_func_array('array_merge',array_map(function($v)use($errors){
+        return $errors->get($v);
+        },
+        array_map(function($k,$v){
+            return "{$v}{$k}";
+            },array_keys($errorKey),
+            $errorKey)))) as $message)
+        <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> {{$message}}</label><br/>
     @endforeach
 @else
     @if($errors->has($errorKey))
