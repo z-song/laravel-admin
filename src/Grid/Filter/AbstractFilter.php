7caa0f53a757eb266bf7e479da8e81b1881cee24
xiaohui.lam <xiaohui.lam@icloud.com>
2019-04-03 15:54:37 +0800

    multipleSeletct/...  methods support collection

--- src/Grid/Filter/AbstractFilter.php
+++ src/Grid/Filter/AbstractFilter.php
@@ -248,7 +248,7 @@ abstract class AbstractFilter
     }
 
     /**
-     * @param array $options
+     * @param array|\Illuminate\Support\Collection $options
      *
      * @return MultipleSelect
      */
@@ -258,7 +258,7 @@ abstract class AbstractFilter
     }
 
     /**
-     * @param array $options
+     * @param array|\Illuminate\Support\Collection $options
      *
      * @return Radio
      */
@@ -268,7 +268,7 @@ abstract class AbstractFilter
     }
 
     /**
-     * @param array $options
+     * @param array|\Illuminate\Support\Collection $options
      *
      * @return Checkbox
      */
@@ -280,7 +280,7 @@ abstract class AbstractFilter
     /**
      * Datetime filter.
      *
-     * @param array $options
+     * @param array|\Illuminate\Support\Collection $options
      *
      * @return DateTime
      */
