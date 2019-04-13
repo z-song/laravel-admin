a1875926add1a138b5d50fc4056ef2409bc69489
SSpirits <admin@sspirits.top>
2019-04-13 23:33:20 +0800

    fix null exception

--- resources/views/form/multipleselect.blade.php
+++ resources/views/form/multipleselect.blade.php
@@ -8,7 +8,7 @@
 
         <select class="form-control {{$class}}" style="width: 100%;" name="{{$name}}[]" multiple="multiple" data-placeholder="{{ $placeholder }}" {!! $attributes !!} >
             @foreach($options as $select => $option)
-                <option value="{{$select}}" {{  in_array($select, (array)old($column, array_filter($value))) ?'selected':'' }}>{{$option}}</option>
+                <option value="{{$select}}" {{  in_array($select, (array)old($column, array_filter((array)$value))) ?'selected':'' }}>{{$option}}</option>
             @endforeach
         </select>
         <input type="hidden" name="{{$name}}[]" />
