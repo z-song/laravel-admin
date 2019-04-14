ccc99a52b9dcc837b9a4d6084662b60810d73ac6
z-song <zosong@126.com>
2019-04-14 16:57:25 +0800

    Optimize the display of exception trace.

--- resources/views/partials/exception.blade.php
+++ resources/views/partials/exception.blade.php
@@ -4,16 +4,11 @@
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         <h4>
             <i class="icon fa fa-warning"></i>
-            <i style="border-bottom: 1px dotted #fff;cursor: pointer;" title="{{ $error->get('type')[0] }}" ondblclick="var f=this.innerHTML;this.innerHTML=this.title;this.title=f;">{{ class_basename($error->get('type')[0]) }}</i>
-            In <i title="{{ $error->get('file')[0] }} line {{ $error->get('line')[0] }}" style="border-bottom: 1px dotted #fff;cursor: pointer;" ondblclick="var f=this.innerHTML;this.innerHTML=this.title;this.title=f;">{{ basename($error->get('file')[0]) }} line {{ $error->get('line')[0] }}</i> :
+            <i style="border-bottom: 1px dotted #fff;cursor: pointer;" title="{{ $error->first('type') }}" ondblclick="var f=this.innerHTML;this.innerHTML=this.title;this.title=f;">{{ class_basename($error->first('type')) }}</i>
+            In <i title="{{ $error->first('file') }} line {{ $error->first('line') }}" style="border-bottom: 1px dotted #fff;cursor: pointer;" ondblclick="var f=this.innerHTML;this.innerHTML=this.title;this.title=f;">{{ basename($error->first('file')) }} line {{ $error->first('line') }}</i> :
         </h4>
-        <p>{!! $error->get('message')[0] !!}</p>
-        <br>
-        <span onclick="$('#laravel-admin-exception-trace').toggleClass('hidden')" style="cursor: pointer">显示/隐藏堆栈信息</span>
-        <ul id="laravel-admin-exception-trace" class="hidden">
-            @foreach($error->get('trace') as $item)
-                <li>{{ $item }}</li>
-            @endforeach
-        </ul>
+        <p><a style="cursor: pointer;" onclick="$('#laravel-admin-exception-trace').toggleClass('hidden');$('i', this).toggleClass('fa-angle-double-down fa-angle-double-up');"><i class="fa fa-angle-double-down"></i>&nbsp;&nbsp;{!! $error->first('message') !!}</a></p>
+
+        <p class="hidden" id="laravel-admin-exception-trace"><br>{!! nl2br($error->first('trace')) !!}</p>
     </div>
 @endif
