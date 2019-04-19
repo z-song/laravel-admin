29d30d171d7f601b308548b370d4ac7738344101
z-song <zosong@126.com>
2019-04-19 11:04:45 +0800

    Optimize the style of checkbox and radio

--- resources/views/form/radio.blade.php
+++ resources/views/form/radio.blade.php
@@ -7,11 +7,15 @@
         @include('admin::form.error')
 
         @foreach($options as $option => $label)
-            @if(!$inline)<div class="radio icheck">@endif
+
+            {!! $inline ? '<span class="icheck">' : '<div class="radio icheck">'  !!}
+
                 <label @if($inline)class="radio-inline"@endif>
                     <input type="radio" name="{{$name}}" value="{{$option}}" class="minimal {{$class}}" {{ ($option == old($column, $value)) || ($value === null && in_array($label, $checked)) ?'checked':'' }} {!! $attributes !!} />&nbsp;{{$label}}&nbsp;&nbsp;
                 </label>
-            @if(!$inline)</div>@endif
+
+            {!! $inline ? '</span>' :  '</div>' !!}
+
         @endforeach
 
         @include('admin::form.help-block')
