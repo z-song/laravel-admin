44bd27568ea5b295bae0c1491e3324be9b7bbbe9
Edwin Hui <xch_566@163.com>
2019-05-21 18:52:47 +0800

    增加一个参数，避免一对多建立空数据时，按钮无数据。

--- src/Grid/Displayers/Expand.php
+++ src/Grid/Displayers/Expand.php
@@ -6,7 +6,7 @@ use Encore\Admin\Admin;
 
 class Expand extends AbstractDisplayer
 {
-    public function display($callback = null)
+    public function display($callback = null, $def = '')
     {
         $callback = $callback->bindTo($this->row);
 
@@ -16,14 +16,15 @@ class Expand extends AbstractDisplayer
 
         $rowKey = $this->getKey();
         $key = uniqid();
+        $val = $this->value ?: $def;
 
         return <<<EOT
 <style>
   .grid-expand:before{content:"\\f103";display: inline-block;font: normal normal normal 14px/1 FontAwesome;font-size: inherit;text-rendering: auto;-webkit-font-smoothing: antialiased;}
   .grid-expand.collapsed:before{content:"\\f102";}
 </style>
-<a class="grid-expand collapsed" data-inserted="0" data-key="{$key}" data-row-key="{$rowKey}" data-parent="grid-expand-{$rowKey}" data-toggle="collapse" data-target="#grid-collapse-{$key}">
-   &nbsp;&nbsp;{$this->value}
+<a href="javascript:void(0)" class="grid-expand collapsed" data-inserted="0" data-key="{$key}" data-row-key="{$rowKey}" data-parent="grid-expand-{$rowKey}" data-toggle="collapse" data-target="#grid-collapse-{$key}">
+   &nbsp;&nbsp;{$val}
 </a>
 <template class="grid-expand-{$key}">
     <div id="grid-collapse-{$key}" class="collapse grid-expand-{$rowKey}">
