8c2437839e72cd02ad165b54b172f2c498af7dc6
zeroiszero <chernglin1122@gmail.com>
2019-06-03 12:34:12 +0800

    allow save images in multiple sizes

--- src/Form/Field/ImageField.php
+++ src/Form/Field/ImageField.php
@@ -2,7 +2,10 @@
 
 namespace Encore\Admin\Form\Field;
 
+use Intervention\Image\Constraint;
+use Intervention\Image\Facades\Image as InterventionImage;
 use Intervention\Image\ImageManagerStatic;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 trait ImageField
 {
@@ -12,6 +15,13 @@ trait ImageField
      * @var array
      */
     protected $interventionCalls = [];
+
+    /**
+     * Thumbnail sizes.
+     *
+     * @var array
+     */
+    protected $sizes = [];
 
     /**
      * Default directory for file to upload.
@@ -84,5 +94,79 @@ trait ImageField
         $this->options(['allowedFileTypes' => ['image']]);
 
         return parent::render();
+    }
+
+    /**
+     * @param string $name
+     * @param int    $width
+     * @param int    $height
+     *
+     * @return $this
+     */
+    public function addThumbnailSize(string $name, int $width, int $height)
+    {
+        $this->sizes[] = compact('name', 'width', 'height');
+
+        return $this;
+    }
+
+    /**
+     * Destroy original thumbnail files.
+     *
+     * @return void.
+     */
+    public function destroyThumbnail()
+    {
+        foreach ($this->sizes as $size) {
+            // We need to get extension type ( .jpeg , .png ...)
+            $ext = pathinfo($this->original, PATHINFO_EXTENSION);
+
+            // We remove extension from file name so we can append thumbnail type
+            $name = str_replace_last('.'.$ext, '', $this->original);
+
+            // We merge original name + thumbnail name + extension
+            $name = $name.'-'.$size['name'].'.'.$ext;
+
+            if ($this->storage->exists($name)) {
+                $this->storage->delete($name);
+            }
+        }
+    }
+
+    /**
+     * Upload file and delete original thumbnail files.
+     *
+     * @param UploadedFile $file
+     *
+     * @return $this
+     */
+    protected function uploadAndDeleteOriginalThumbnail(UploadedFile $file)
+    {
+        foreach ($this->sizes as $size) {
+            // We need to get extension type ( .jpeg , .png ...)
+            $ext = pathinfo($this->name, PATHINFO_EXTENSION);
+
+            // We remove extension from file name so we can append thumbnail type
+            $name = str_replace_last('.'.$ext, '', $this->name);
+
+            // We merge original name + thumbnail name + extension
+            $name = $name.'-'.$size['name'].'.'.$ext;
+
+            // Resize image with aspect ratio
+            $image = InterventionImage::make($file);
+            $image->resize($size['width'], $size['height'], function (Constraint $constraint) {
+                $constraint->aspectRatio();
+            });
+
+            if (!is_null($this->storage_permission)) {
+                $this->storage->put("{$this->getDirectory()}/{$name}", $image->encode(), $this->storage_permission);
+            } else {
+                $this->storage->put("{$this->getDirectory()}/{$name}", $image->encode());
+            }
+        }
+
+        $this->destroyThumbnail();
+
+        return $this;
     }
 }
---
+++ src/Traits/Resizable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Encore\Admin\Traits;
+
+trait Resizable
+{
+    /**
+     * Method for returning specific thumbnail for model.
+     *
+     * @param string $type
+     * @param string $attribute
+     *
+     * @return string
+     */
+    public function thumbnail($type, $attribute = 'image')
+    {
+        // Return empty string if the field not found
+        if (!isset($this->attributes[$attribute])) {
+            return '';
+        }
+
+        // We take image from posts field
+        $image = $this->attributes[$attribute];
+
+        $thumbnail = $this->getThumbnail($image, $type);
+
+        return \Illuminate\Support\Facades\Storage::exists($thumbnail) ? $thumbnail : $image;
+    }
+
+    /**
+     * Generate thumbnail URL.
+     *
+     * @param $image
+     * @param $type
+     *
+     * @return string
+     */
+    public function getThumbnail($image, $type)
+    {
+        // We need to get extension type ( .jpeg , .png ...)
+        $ext = pathinfo($image, PATHINFO_EXTENSION);
+
+        // We remove extension from file name so we can append thumbnail type
+        $name = str_replace_last('.'.$ext, '', $image);
+
+        // We merge original name + type + extension
+        return $name.'-'.$type.'.'.$ext;
+    }
+}
