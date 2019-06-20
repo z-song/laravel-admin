5ca1684933e905c7235d7dbad4f69fb796562437
Taha Moghaddam <m5taha5m@gmail.com>
2019-06-21 01:35:34 +0430

    Show ignored fields value on forms
    When you ignore a field like a hasMany field you would not able to see fields and values. But now you can see hasMany fields and values but you will not be able to change them except you use field's data on your `$from->saving()` or `$form->saved()` callbacks.

--- src/Form.php
+++ src/Form.php
@@ -1068,9 +1068,7 @@ class Form implements Renderable
         $data = $this->model->toArray();
 
         $this->builder->fields()->each(function (Field $field) use ($data) {
-            if (!in_array($field->column(), $this->ignored)) {
-                $field->fill($data);
-            }
+            $field->fill($data);
         });
     }
 
