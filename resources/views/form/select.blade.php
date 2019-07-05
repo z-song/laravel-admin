90d59999d94c16615dd279a94401fd35370e8bb4
Dena <dena.ajdari@gmail.com>
2019-07-05 19:53:06 +0430

    Fix 0 key and Null conflict on Select and Multiple Select.

--- resources/views/form/select.blade.php
+++ resources/views/form/select.blade.php
@@ -13,14 +13,14 @@
                 @foreach($groups as $group)
                     <optgroup label="{{ $group['label'] }}">
                         @foreach($group['options'] as $select => $option)
-                            <option value="{{$select}}" {{ $select == old($column, $value) ?'selected':'' }}>{{$option}}</option>
+                            <option value="{{$select}}" {{ $select === old($column, $value) ?'selected':'' }}>{{$option}}</option>
                         @endforeach
                     </optgroup>
                 @endforeach
              @else
                 <option value=""></option>
                 @foreach($options as $select => $option)
-                    <option value="{{$select}}" {{ $select == old($column, $value) ?'selected':'' }}>{{$option}}</option>
+                    <option value="{{$select}}" {{ $select === old($column, $value) ?'selected':'' }}>{{$option}}</option>
                 @endforeach
             @endif
         </select>
