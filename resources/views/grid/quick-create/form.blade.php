c01a9e1ead0a22cef6d72846c5466191e7174ffe
z-song <zosong@126.com>
2019-07-12 15:07:14 +0800

    wip

--- resources/views/grid/quick-create/form.blade.php
+++ resources/views/grid/quick-create/form.blade.php
@@ -3,7 +3,7 @@
     <td colspan="{{ $columnCount }}" style="height: 47px;padding-left: 57px;background-color: #f9f9f9; vertical-align: middle;">
 
         <span class="create" style="color: #bdbdbd;cursor: pointer;display: block;">
-             <i class="fa fa-plus"></i>&nbsp;{{ __('admin.quick-create') }}
+             <i class="fa fa-plus"></i>&nbsp;{{ __('admin.quick_create') }}
         </span>
 
         <form class="form-inline create-form" style="display: none;" method="post">
