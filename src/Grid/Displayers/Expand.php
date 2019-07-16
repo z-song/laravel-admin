a5bb26e722150c1a46b3d825eacdc3bd1fd7f29c
cheerego <41974873@qq.com>
2019-07-16 14:51:09 +0800

    fix expand

--- src/Grid/Displayers/Expand.php
+++ src/Grid/Displayers/Expand.php
@@ -6,21 +6,25 @@ use Encore\Admin\Admin;
 
 class Expand extends AbstractDisplayer
 {
+    private $uniqueKey;
+
     public function display($callback = null)
     {
         $callback = $callback->bindTo($this->row);
 
         $html = call_user_func_array($callback, [$this->row]);
 
+        $key = $this->getKey();
+
+        $this->uniqueKey = $key . '-' . $this->column->getName();
+
         $this->setupScript();
 
-        $key = $this->column->getName().'-'.$this->getKey();
-
         return <<<EOT
-<span class="grid-expand" data-inserted="0" data-key="{$key}" data-toggle="collapse" data-target="#grid-collapse-{$key}">
+<span class="grid-expand-{$this->uniqueKey}" data-inserted="0" data-key="{$key}" data-toggle="collapse" data-target="#grid-collapse-{$key}">
    <a href="javascript:void(0)"><i class="fa fa-angle-double-down"></i>&nbsp;&nbsp;{$this->value}</a>
 </span>
-<template class="grid-expand-{$key}">
+<template class="grid-expand-{$this->uniqueKey}">
     <div id="grid-collapse-{$key}" class="collapse">
         <div  style="padding: 10px 10px 0 10px;">$html</div>
     </div>
@@ -30,15 +34,15 @@ EOT;
 
     protected function setupScript()
     {
-        $script = <<<'EOT'
+        $script = <<<EOT
 
-$('.grid-expand').on('click', function () {
+$('.grid-expand-{$this->uniqueKey}').on('click', function () {
     
     if ($(this).data('inserted') == '0') {
     
         var key = $(this).data('key');
         var row = $(this).closest('tr');
-        var html = $('template.grid-expand-'+key).html();
+        var html = $('template.grid-expand-'+"$this->uniqueKey").html();
 
         row.after("<tr style='background-color: #ecf0f5;'><td colspan='"+(row.find('td').length)+"' style='padding:0 !important; border:0;'>"+html+"</td></tr>");
 
