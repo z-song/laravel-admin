f497d06b03db02d177cf9e87e471fe725d37b2c0
z-song <zosong@126.com>
2019-07-31 12:16:21 +0800

    refactoring

--- src/Grid/Displayers/Expand.php
+++ src/Grid/Displayers/Expand.php
@@ -6,18 +6,18 @@ use Encore\Admin\Admin;
 
 class Expand extends AbstractDisplayer
 {
-    public function display($callback = null, $isExpand = false)
+    public function display(\Closure $callback = null, $isExpand = false)
     {
         $callback = $callback->bindTo($this->row);
 
         $html = call_user_func_array($callback, [$this->row]);
 
-        $this->setupScript($isExpand);
+        $this->addScript($isExpand);
 
         $key = $this->column->getName().'-'.$this->getKey();
 
         return <<<EOT
-<span class="grid-expand-{$this->grid->getGridRowName()}" data-inserted="0" data-key="{$key}" data-toggle="collapse" data-target="#grid-collapse-{$key}">
+<span class="{$this->getElementClass()}" data-inserted="0" data-key="{$key}" data-toggle="collapse" data-target="#grid-collapse-{$key}">
    <a href="javascript:void(0)"><i class="fa fa-angle-double-down"></i>&nbsp;&nbsp;{$this->value}</a>
 </span>
 <template class="grid-expand-{$key}">
@@ -28,11 +28,11 @@ class Expand extends AbstractDisplayer
 EOT;
     }
 
-    protected function setupScript($isExpand)
+    protected function addScript($isExpand)
     {
         $script = <<<EOT
 
-$('.grid-expand-{$this->grid->getGridRowName()}').on('click', function () {
+$('.{$this->getElementClass()}').on('click', function () {
     
     if ($(this).data('inserted') == '0') {
     
@@ -49,9 +49,18 @@ $('.grid-expand-{$this->grid->getGridRowName()}').on('click', function () {
 });
 EOT;
 
-    if($isExpand) {
-        $script .= "$('.grid-expand-{$this->grid->getGridRowName()}').trigger('click');";
-    }
+        if($isExpand) {
+            $script .= "$('.{$this->getElementClass()}').trigger('click');";
+        }
+
         Admin::script($script);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getElementClass()
+    {
+        return "grid-expand-{$this->grid->getGridRowName()}";
     }
 }
