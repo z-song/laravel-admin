a08ca992caf4633ded94df5228ad486fdeb43568
z-song <zosong@126.com>
2019-08-01 11:46:36 +0800

    Fix #3736

--- src/Grid/Tools/FixColumns.php
+++ src/Grid/Tools/FixColumns.php
@@ -96,7 +96,10 @@ class FixColumns
      */
     protected function addScript()
     {
+        $allName = $this->grid->getSelectAllName();
         $rowName = $this->grid->getGridRowName();
+
+        $selected = trans('admin.grid_items_selected');
 
         $script = <<<SCRIPT
 
@@ -150,6 +153,25 @@ class FixColumns
             $('.table-fixed-left tbody tr').eq(index).css('background-color', '');
             $('.table-fixed-right tbody tr').eq(index).css('background-color', '');
         }
+    }).on('ifClicked', function () {
+    
+        var id = $(this).data('id');
+        
+        if (this.checked) {
+            $.admin.grid.unselect(id);
+        } else {
+            $.admin.grid.select(id);
+        }
+        
+        var selected = $.admin.grid.selected().length;
+        
+        if (selected > 0) {
+            $('.{$allName}-btn').show();
+        } else {
+            $('.{$allName}-btn').hide();
+        }
+        
+        $('.{$allName}-btn .selected').html("{$selected}".replace('{n}', selected));
     });
 })();
 
