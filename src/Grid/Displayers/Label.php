6984e9d391ba56c348f390e94a3dbfa21b9a3f3f
lixu521 <1351343400@qq.com>
2019-08-07 14:17:32 +0800

    兼容了数据模型关联对象的时候，由于返回的不是指定值，导致关联对象取值错误的问题

--- src/Grid/Displayers/Label.php
+++ src/Grid/Displayers/Label.php
@@ -16,7 +16,6 @@ class Label extends AbstractDisplayer
         return collect((array) $this->value)->map(function ($item) use ($style) {
             if (is_array($style)) {
                 if(is_string($this->getColumn()->getOriginal()) || is_int($this->getColumn()->getOriginal())) {
-                    var_dump($this->getColumn()->getOriginal());
                     $style = Arr::get($style, $this->getColumn()->getOriginal(), 'success');
                 } else {
                     $style = Arr::get($style, $item, 'success');
