506c69d0d26f5b5e1e4a92032b6a67d47f3a2478
toknsiT <toknsiT@outlook.com>
2019-08-07 11:37:15 +0800

    add except-fields option

--- src/Console/ExportSeedCommand.php
+++ src/Console/ExportSeedCommand.php
@@ -13,7 +13,8 @@ class ExportSeedCommand extends Command
      * @var string
      */
     protected $signature = 'admin:export-seed {classname=AdminTablesSeeder}
-                                              {--users : add to seed users tables}';
+                                              {--users : add to seed users tables}
+                                              {--except-fields=id,created_at,updated_at : except fields}';
 
     /**
      * The console command description.
@@ -30,7 +31,7 @@ class ExportSeedCommand extends Command
     public function handle()
     {
         $name = $this->argument('classname');
-        $exceptFields = [];
+        $exceptFields = explode(',', $this->option('except-fields'));
         $exportUsers = $this->option('users');
 
         $seedFile = $this->laravel->databasePath().'/seeds/'.$name.'.php';
