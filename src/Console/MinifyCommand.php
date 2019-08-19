ee487fd8040991a7b8ac39b897e890aa1d5084a2
z-song <zosong@126.com>
2019-08-19 14:54:59 +0800

    wip

--- src/Console/MinifyCommand.php
+++ src/Console/MinifyCommand.php
@@ -52,9 +52,9 @@ class MinifyCommand extends Command
             return $this->clearMinifiedFiles();
         }
 
-        $this->loadExcepts();
-
         AdminFacade::bootstrap();
+
+        $this->loadExcepts();
 
         $this->minifyCSS();
         $this->minifyJS();
@@ -73,11 +73,9 @@ class MinifyCommand extends Command
 
     protected function loadExcepts()
     {
-        $excepts = config('admin.minify_assets.excepts');
+        $excepts = config('admin.minify_assets.excepts', []);
 
-        if (is_array($excepts)) {
-            $this->excepts = array_filter($excepts);
-        }
+        $this->excepts = array_merge($excepts, Admin::$minifyIgnores);
     }
 
     protected function clearMinifiedFiles()
