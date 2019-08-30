c7ba1a084e16834bdf4af5c857ff95f9277c93a3
lixu521 <1351343400@qq.com>
2019-08-30 17:48:26 +0800

    解决在expand内插入的内容无法绑定事件的问题

--- src/Grid/Displayers/Expand.php
+++ src/Grid/Displayers/Expand.php
@@ -20,11 +20,9 @@ class Expand extends AbstractDisplayer
 <span class="{$this->getElementClass()}" data-inserted="0" data-key="{$key}" data-toggle="collapse" data-target="#grid-collapse-{$key}">
    <a href="javascript:void(0)"><i class="fa fa-angle-double-down"></i>&nbsp;&nbsp;{$this->value}</a>
 </span>
-<template class="grid-expand-{$key}">
-    <div id="grid-collapse-{$key}" class="collapse">
-        <div  style="padding: 10px 10px 0 10px;">$html</div>
-    </div>
-</template>
+<div id="grid-collapse-{$key}" class="collapse">
+    <div  style="padding: 10px 10px 0 10px;">$html</div>
+</div>
 EOT;
     }
 
@@ -38,10 +36,10 @@ $('.{$this->getElementClass()}').on('click', function () {
     
         var key = $(this).data('key');
         var row = $(this).closest('tr');
-        var html = $('template.grid-expand-'+key).html();
-
-        row.after("<tr style='background-color: #ecf0f5;'><td colspan='"+(row.find('td').length)+"' style='padding:0 !important; border:0;'>"+html+"</td></tr>");
-
+        
+        row.after("<tr style='background-color: #ecf0f5;'><td id='expand-" + key + "' colspan='" + (row.find('td').length) + "' style='padding:0 !important; border:0;'></td></tr>");
+        $('#grid-collapse-' + key).appendTo($("#expand-" + key));
+        
         $(this).data('inserted', 1);
     }
     
