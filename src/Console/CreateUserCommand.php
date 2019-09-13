c4ad42e8753ef5642c94a79ca9259829041e6786
John Zhang <zsgsdesign@gmail.com>
2019-09-13 20:55:48 +0800

    add judge for those disable roles

--- src/Console/CreateUserCommand.php
+++ src/Console/CreateUserCommand.php
@@ -37,17 +37,23 @@ class CreateUserCommand extends Command
         $roles = $roleModel::all();
 
         /** @var array $selected */
-        $selected = $this->choice('Please choose a role for the user', $roles->pluck('name')->toArray(), null, null, true);
+        $selectedOption = $roles->pluck('name')->toArray();
+        
+        if(empty($selectedOption)) { 
+            $selected = $this->choice('Please choose a role for the user', $selectedOption, null, null, true);
 
-        $roles = $roles->filter(function ($role) use ($selected) {
-            return in_array($role->name, $selected);
-        });
+            $roles = $roles->filter(function ($role) use ($selected) {
+                return in_array($role->name, $selected);
+            });
+        }
 
         $user = new $userModel(compact('username', 'password', 'name'));
 
         $user->save();
 
-        $user->roles()->attach($roles);
+        if(isset($roles)) { 
+            $user->roles()->attach($roles);
+        }
 
         $this->info("User [$name] created successfully.");
     }
