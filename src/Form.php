36bb2a4a20d88935e4e160fd679a24b52513f391
Terry <jianhua@heyunshuju.com>
2019-09-14 18:09:56 +0800

    修正关联关系名称为多单词时无法读取关联数据的问题

--- src/Form.php
+++ src/Form.php
@@ -558,6 +558,7 @@ class Form implements Renderable
         $relations = [];
 
         foreach ($inputs as $column => $value) {
+            $column = \Illuminate\Support\Str::camel($column);
             if (!method_exists($this->model, $column)) {
                 continue;
             }
