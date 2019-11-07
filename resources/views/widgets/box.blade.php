e6e939a8505660304b09140d4ff6a26bf8278802
Muhlis Abdi <35951442+muhlisabdi@users.noreply.github.com>
2019-11-07 19:50:03 +0800

    Update box.blade.php

--- resources/views/widgets/box.blade.php
+++ resources/views/widgets/box.blade.php
@@ -1,7 +1,7 @@
 <div {!! $attributes !!}>
     @if($title || $tools)
         <div class="box-header with-border">
-            <h3 class="box-title">{{ $title }}</h3>
+            <h3 class="box-title">{!! $title !!}</h3>
             <div class="box-tools pull-right">
                 @foreach($tools as $tool)
                     {!! $tool !!}
