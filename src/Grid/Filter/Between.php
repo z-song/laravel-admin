e2978cfb8f66bcc8b17e52eca71f34c16486923e
Muhlis Abdi <35951442+muhlisabdi@users.noreply.github.com>
2020-01-02 18:57:01 +0800

    Update Between.php

--- src/Grid/Filter/Between.php
+++ src/Grid/Filter/Between.php
@@ -1,17 +1,13 @@
 <?php
-
 namespace Encore\Admin\Grid\Filter;
-
 use Encore\Admin\Admin;
 use Illuminate\Support\Arr;
-
 class Between extends AbstractFilter
 {
     /**
      * {@inheritdoc}
      */
     protected $view = 'admin::filter.between';
-
     /**
      * Format id.
      *
@@ -22,10 +18,8 @@ class Between extends AbstractFilter
     public function formatId($column)
     {
         $id = str_replace('.', '_', $column);
-
         return ['start' => "{$id}_start", 'end' => "{$id}_end"];
     }
-
     /**
      * Format two field names of this filter.
      *
@@ -36,20 +30,16 @@ class Between extends AbstractFilter
     protected function formatName($column)
     {
         $columns = explode('.', $column);
-
         if (count($columns) == 1) {
             $name = $columns[0];
         } else {
             $name = array_shift($columns);
-
             foreach ($columns as $column) {
                 $name .= "[$column]";
             }
         }
-
         return ['start' => "{$name}[start]", 'end' => "{$name}[end]"];
     }
-
     /**
      * Get condition of this filter.
      *
@@ -59,38 +49,28 @@ class Between extends AbstractFilter
      */
     public function condition($inputs)
     {
-        if (! Arr::has($inputs, $this->column)) {
         if ($this->ignore) {
             return;
         }
-
-        if (! Arr::has($inputs, $this->column)) {
+        if (!Arr::has($inputs, $this->column)) {
             return;
         }
-
         $this->value = Arr::get($inputs, $this->column);
-
         $value = array_filter($this->value, function ($val) {
             return $val !== '';
         });
-
         if (empty($value)) {
             return;
         }
-
-        if (! isset($value['start'])) {
+        if (!isset($value['start'])) {
             return $this->buildCondition($this->column, '<=', $value['end']);
         }
-
-        if (! isset($value['end'])) {
+        if (!isset($value['end'])) {
             return $this->buildCondition($this->column, '>=', $value['start']);
         }
-
         $this->query = 'whereBetween';
-
         return $this->buildCondition($this->column, $this->value);
     }
-
     /**
      * @param array $options
      *
@@ -99,12 +79,9 @@ class Between extends AbstractFilter
     public function datetime($options = [])
     {
         $this->view = 'admin::filter.betweenDatetime';
-
         $this->setupDatetime($options);
-
         return $this;
     }
-
     /**
      * @param array $options
      */
@@ -112,10 +89,8 @@ class Between extends AbstractFilter
     {
         $options['format'] = Arr::get($options, 'format', 'YYYY-MM-DD HH:mm:ss');
         $options['locale'] = Arr::get($options, 'locale', config('app.locale'));
-
         $startOptions = json_encode($options);
         $endOptions = json_encode($options + ['useCurrent' => false]);
-
         $script = <<<EOT
             $('#{$this->id['start']}').datetimepicker($startOptions);
             $('#{$this->id['end']}').datetimepicker($endOptions);
@@ -126,7 +101,6 @@ class Between extends AbstractFilter
                 $('#{$this->id['start']}').data("DateTimePicker").maxDate(e.date);
             });
 EOT;
-
         Admin::script($script);
     }
 }
