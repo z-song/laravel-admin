17b2d513a472f9c3c0a01161ee5bd604b00ce949
jiayx <i@jiayx.net>
2020-02-01 21:25:55 +0800

    兼容 filter multipleSelect 参数值不为数组的情况，防止页面错乱

--- resources/views/filter/multipleselect.blade.php
+++ resources/views/filter/multipleselect.blade.php
@@ -1,6 +1,11 @@
 <select class="form-control {{ $class }}" name="{{$name}}[]" multiple style="width: 100%;">
     <option></option>
+    @php
+        $selected = request($name, []);
+        $selected = is_array($selected) ? $selected : [];
+    @endphp
+
     @foreach($options as $select => $option)
-        <option value="{{$select}}" {{ in_array((string)$select, request($name, []))  ?'selected':'' }}>{{$option}}</option>
+        <option value="{{$select}}" {{ in_array((string)$select, $selected) ? 'selected' : '' }}>{{$option}}</option>
     @endforeach
 </select>
