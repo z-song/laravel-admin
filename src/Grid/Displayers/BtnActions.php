db8dcf2507b4be42c14ab09eb138d5f3b8f101bd
蒲德军 <xiaozhi2015@hotmail.com>
2020-02-11 17:48:16 +0800

    '表格操作新增按钮模式-遵循弹窗规则'

---
+++ src/Grid/Displayers/BtnActions.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Encore\Admin\Grid\Displayers;
+
+
+use Encore\Admin\Actions\RowAction;
+use Encore\Admin\Admin;
+use Encore\Admin\Grid\Actions\Delete;
+use Encore\Admin\Grid\Actions\Edit;
+use Encore\Admin\Grid\Actions\Show;
+
+class BtnActions extends Actions
+{
+    /**
+     * @var array
+     */
+    protected $custom = [];
+
+    /**
+     * @var array
+     */
+    protected $default = [];
+
+    /**
+     * @var array
+     */
+    protected $defaultClass = [Edit::class, Show::class, Delete::class];
+
+    /**
+     * Add JS script into pages.
+     *
+     * @return void.
+     */
+    protected function addScript()
+    {
+        $script = <<<'SCRIPT'
+$(function() {
+    $('td.column-__actions__ a').addClass('btn btn-xs btn-default').css({'margin-right':'5px'});
+});
+SCRIPT;
+
+        Admin::script($script);
+    }
+
+    /**
+     * @param RowAction $action
+     *
+     * @return $this
+     */
+    public function add(RowAction $action)
+    {
+        $this->prepareAction($action);
+
+        array_push($this->custom, $action);
+
+        return $this;
+    }
+
+    /**
+     * Prepend default `edit` `view` `delete` actions.
+     */
+    protected function prependDefaultActions()
+    {
+        foreach ($this->defaultClass as $class) {
+            /** @var RowAction $action */
+            $action = new $class();
+
+            $this->prepareAction($action);
+
+            array_push($this->default, $action);
+        }
+    }
+
+    /**
+     * @param RowAction $action
+     */
+    protected function prepareAction(RowAction $action)
+    {
+        $action->setGrid($this->grid)
+            ->setColumn($this->column)
+            ->setRow($this->row);
+    }
+
+    /**
+     * Disable view action.
+     *
+     * @param bool $disable
+     *
+     * @return $this
+     */
+    public function disableView(bool $disable = true)
+    {
+        if ($disable) {
+            array_delete($this->defaultClass, Show::class);
+        } elseif (!in_array(Show::class, $this->defaultClass)) {
+            array_push($this->defaultClass, Show::class);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Disable delete.
+     *
+     * @param bool $disable
+     *
+     * @return $this.
+     */
+    public function disableDelete(bool $disable = true)
+    {
+        if ($disable) {
+            array_delete($this->defaultClass, Delete::class);
+        } elseif (!in_array(Delete::class, $this->defaultClass)) {
+            array_push($this->defaultClass, Delete::class);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Disable edit.
+     *
+     * @param bool $disable
+     *
+     * @return $this
+     */
+    public function disableEdit(bool $disable = true)
+    {
+        if ($disable) {
+            array_delete($this->defaultClass, Edit::class);
+        } elseif (!in_array(Edit::class, $this->defaultClass)) {
+            array_push($this->defaultClass, Edit::class);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param null|\Closure $callback
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|string
+     */
+    public function display($callback = null)
+    {
+        $this->addScript();
+
+        if ($callback instanceof \Closure) {
+            $callback->call($this, $this);
+        }
+
+        if ($this->disableAll) {
+            return '';
+        }
+
+        $this->prependDefaultActions();
+
+        $actions = '';
+        foreach ($this->default as $action) {
+            $actions .= $action->render();
+        }
+        if(!empty($this->custom)) {
+            foreach($this->custom as $action) {
+                $actions .= $action->render();
+            }
+        }
+        return $actions;
+    }
+}
