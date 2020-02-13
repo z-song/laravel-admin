daa032c8da70dec154ead159dff9d4f0657f0981
Robert Tirtasentana <robertwt7@gmail.com>
2020-02-14 09:27:16 +1100

    Fixed icon in menu controller

--- src/Controllers/MenuController.php
+++ src/Controllers/MenuController.php
@@ -75,7 +75,7 @@ class MenuController extends Controller
             $tree->disableCreate();
 
             $tree->branch(function ($branch) {
-                $payload = "<i class='fa {$branch['icon']}'></i>&nbsp;<strong>{$branch['title']}</strong>";
+                $payload = "<i class='{$branch['icon']}'></i>&nbsp;<strong>{$branch['title']}</strong>";
 
                 if (! isset($branch['children'])) {
                     if (url()->isValidUrl($branch['uri'])) {
