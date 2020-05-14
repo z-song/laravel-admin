4ca5834445fb1872836ee6b33798518f3b6fd365
z-song <zosong@126.com>
2020-05-14 12:52:57 +0800

    refactoring grid column select

--- src/Grid/Displayers/Select.php
+++ src/Grid/Displayers/Select.php
@@ -6,25 +6,23 @@ use Encore\Admin\Admin;
 
 class Select extends AbstractDisplayer
 {
-    public function display($options = [])
+    protected function addScript()
     {
-        if ($options instanceof \Closure) {
-            $options = $options->call($this, $this->row);
-        }
-
         $name = $this->column->getName();
 
         $class = "grid-select-{$name}";
 
         $script = <<<EOT
 
-$('.$class').select2().on('change', function(){
+$('.$class .dropdown-menu li a').click(function () {
+    var target = $(this);
 
-    var pk = $(this).data('key');
-    var value = $(this).val();
+    var text = target.text();
+    var value = target.data('value');
+    var key = target.parents('.$class').attr('key');
 
     $.ajax({
-        url: "{$this->grid->resource()}/" + pk,
+        url: "{$this->grid->resource()}/" + key,
         type: "POST",
         data: {
             $name: value,
@@ -32,6 +30,16 @@ $('.$class').select2().on('change', function(){
             _method: 'PUT'
         },
         success: function (data) {
+            target.parents('.$class').find('.select-text').text(text);
+
+            target.parents('.dropdown-menu').find('li a').each(function () {
+                if (value == $(this).data('value')) {
+                    $(this).find('i').removeClass('invisible')
+                } else {
+                    $(this).find('i').addClass('invisible')
+                }
+            });
+
             toastr.success(data.message);
         }
     });
@@ -40,21 +48,48 @@ $('.$class').select2().on('change', function(){
 EOT;
 
         Admin::script($script);
+    }
 
-        $key = $this->row->{$this->grid->getKeyName()};
+    protected function addStyle()
+    {
+        $style = <<<STYLE
+.grid-select-{$this->column->getName()} .dropdown-menu {
+    min-width: 0px;
+}
+STYLE;
+
+        Admin::style($style);
+    }
+
+    public function display($options = [])
+    {
+        $this->addScript();
+        $this->addStyle();
+
+        $name = $this->column->getName();
+
+        if ($options instanceof \Closure) {
+            $options = $options->call($this, $this->row);
+        }
 
         $optionsHtml = '';
 
         foreach ($options as $option => $text) {
-            $selected = $option == $this->value ? 'selected' : '';
-            $optionsHtml .= "<option value=\"$option\" $selected>$text</option>";
+            $invisible = $option == $this->value ? '' : 'invisible';
+            $optionsHtml .= "<li><a href='javascript:void(0);' data-value='{$option}'><i class=\"fa fa-check $invisible\"></i>{$text}</a></li>";
         }
 
-        return <<<EOT
-<select style="width: 100%;" class="$class btn btn-mini" data-key="$key">
-$optionsHtml
-</select>
-
+        return <<<HTML
+<div class="dropdown grid-select-{$this->column->getName()}" key="{$this->getKey()}">
+  <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">
+    <span class="select-text text-muted">{$options[$this->value]}</span>
+    <span class="caret"></span>
+  </a>
+  <ul class="dropdown-menu">
+    {$optionsHtml}
+  </ul>
+</div>
+HTML;
 EOT;
     }
 }
