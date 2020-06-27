73340576072d8a5890cede65874d29c0b280fdb6
Mehmet Hanoğlu <mhanoglu55@gmail.com>
2020-06-27 21:46:47 +0300

    Fix progress bar width in grid

--- src/Grid/Displayers/ProgressBar.php
+++ src/Grid/Displayers/ProgressBar.php
@@ -15,7 +15,7 @@ class ProgressBar extends AbstractDisplayer
         return <<<EOT
 <div class="row" style="min-width: 100px;">
     <span class="col-sm-3" style="color:#777;">{$this->value}%</span>
-    <div class="progress progress-$size col-sm-9" style="padding-left: 0;width: 100px;"> 
+    <div class="progress progress-$size col-sm-9" style="padding-left: 0;padding-right: 0;width: 100px;"> 
         <div class="progress-bar $style" role="progressbar" aria-valuenow="{$this->value}" aria-valuemin="0" aria-valuemax="$max" style="width: {$this->value}%">
         </div>
     </div>
