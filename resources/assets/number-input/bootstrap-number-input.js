617c0e351f2b9c3e6791f1b70c20c09d771435e0
sd-evo <s.dolgopolov1991@zebrains.team>
2020-08-22 04:51:07 +0300

    patch-2
    fix #4810: you can enter a negative number in the form->number field now

--- resources/assets/number-input/bootstrap-number-input.js
+++ resources/assets/number-input/bootstrap-number-input.js
@@ -20,69 +20,88 @@
  */
 
 (function($) {
-
     $.fn.bootstrapNumber = function(options) {
-
-        var settings = $.extend({
+        let settings = $.extend({
             upClass: 'default',
             downClass: 'default',
             center: true
         }, options);
 
-        return this.each(function(e) {
-            var self = $(this);
-            var clone = self.clone();
+        return this.each(function () {
+            let self = $(this);
+            let clone = self.clone();
+            let group = $("<div class='input-group'></div>");
 
-            var min = self.attr('min');
-            var max = self.attr('max');
+            let min = self.attr('min');
+            let max = self.attr('max');
 
-            function setText(n) {
-                n = isNaN(n) ? 0 : n;
-                if ((min && n < min)) {
-                    n = min;
-                } else if (max && n > max) {
-                    n = max;
+            setText(0);
+
+            function setText(number) {
+                if ((min && number < min)) {
+                    number = min;
                 }
-                clone.val(n);
+
+                if (max && number > max) {
+                    number = max;
+                }
+
+                clone.val(number);
             }
 
-            var group = $("<div class='input-group'></div>");
-            var down = $("<button type='button'>-</button>").attr('class', 'btn btn-' + settings.downClass).click(function() {
-                setText(parseInt(clone.val(), 10) - 1);
-                clone.focus().trigger('change');
-            });
-            var up = $("<button type='button'>+</button>").attr('class', 'btn btn-' + settings.upClass).click(function() {
-                setText(parseInt(clone.val(), 10) + 1);
-                clone.focus().trigger('change');
-            });
-            $("<span class='input-group-btn'></span>").append(down).appendTo(group);
+            function fixNumber() {
+                let fixedNumber = clone.val().replace(/(-(-)|(\d+-))|([Na])|(-0).*/, 0);
+                fixedNumber = fixedNumber.replace(/^0(\d+)/, '$1');
+
+                setText(fixedNumber);
+            }
+
+            let buttonMinus = $("<button type='button'>-</button>")
+              .attr('class', 'btn btn-' + settings.downClass)
+              .click(function () {
+                  setText(parseInt(clone.val(), 10) - 1);
+
+                  clone.focus().trigger('change');
+              });
+
+            let buttonPlus = $("<button type='button'>+</button>")
+              .attr('class', 'btn btn-' + settings.upClass)
+              .click(function () {
+                  setText(parseInt(clone.val(), 10) + 1);
+
+                  clone.focus().trigger('change');
+              });
+
+            $("<span class='input-group-btn'></span>").prepend(buttonMinus).appendTo(group);
+
             clone.appendTo(group);
             if (clone) {
                 clone.css('text-align', 'center');
             }
-            $("<span class='input-group-btn'></span>").append(up).appendTo(group);
+
+            $("<span class='input-group-btn'></span>").append(buttonPlus).appendTo(group);
 
             // remove spins from original
             clone.prop('type', 'text').keydown(function(e) {
-                if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 || (e.keyCode == 65 && e.ctrlKey === true) || (e.keyCode >= 35 && e.keyCode <= 39)) {
+                if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1
+                  || (e.keyCode === 65 && e.ctrlKey === true)
+                  || (e.keyCode >= 35 && e.keyCode <= 39)
+                ) {
                     return;
                 }
-                if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+
+                if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57))
+                  && (e.keyCode < 96 || e.keyCode > 105)
+                  && e.keyCode !== 189
+                ) {
                     e.preventDefault();
                 }
-            }).keyup(function(event) {
-                var n = clone.val().match(/\-?\d+/) || [0];
-                setText(n[0]);
-                clone.trigger('change');
-            }).blur(function(e) {
-                var c = String.fromCharCode(e.which);
-                var n = parseInt(clone.val() + c, 10);
-                setText(n);
-                clone.trigger('change');
+            }).bind('keyup keydown blur', function () {
+                fixNumber();
             });
 
 
             self.replaceWith(group);
         });
     };
-}(jQuery));
+}($));
