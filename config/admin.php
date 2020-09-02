a9615a06facdfe669ea09c3c065f93c6985d0355
z-song <zosong@126.com>
2020-09-03 01:33:43 +0800

    wip

--- config/admin.php
+++ config/admin.php
@@ -186,7 +186,7 @@ return [
     | Set a default avatar for newly created users.
     |
     */
-    'default_avatar' => '/vendor/laravel-admin/AdminLTE/dist/img/user2-160x160.jpg',
+    'default_avatar' => '/vendor/laravel-admin/AdminLTE/img/user2-160x160.jpg',
 
     /*
     |--------------------------------------------------------------------------
