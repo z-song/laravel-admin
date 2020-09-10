24eb59a9cfb12c29b15e97c98d8c1eebb27b7d92
z-song <zosong@126.com>
2020-09-10 15:48:51 +0800

    Add access deny page

---
+++ resources/views/pages/deny.blade.php
@@ -0,0 +1,9 @@
+<div class="error-page">
+    <h2 class="headline text-warning"> 503</h2>
+    <div class="error-content pt-3">
+        <h3 class="pt-5">
+            <i class="fas fa-exclamation-triangle text-warning"></i> {{ trans('admin.deny') }}
+        </h3>
+    </div>
+    <!-- /.error-content -->
+</div>
