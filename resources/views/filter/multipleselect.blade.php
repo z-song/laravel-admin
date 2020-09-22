5b472e3ef342ace369f13bd6023e93976224c1a6
Ryohei Endo <r-endo@studio-spoon.co.jp>
2020-09-22 21:05:22 +0900

    Fix multiple select filter

--- resources/views/filter/multipleselect.blade.php
+++ resources/views/filter/multipleselect.blade.php
@@ -1,6 +1,6 @@
 <select class="form-control {{ $class }}" name="{{$name}}[]" multiple style="width: 100%;">
     <option></option>
     @foreach($options as $select => $option)
-        <option value="{{$select}}" {{ in_array((string)$select, request($name, []))  ?'selected':'' }}>{{$option}}</option>
+        <option value="{{$select}}" {{ in_array((string)$select, (array)$value) ?'selected':'' }}>{{$option}}</option>
     @endforeach
 </select>
