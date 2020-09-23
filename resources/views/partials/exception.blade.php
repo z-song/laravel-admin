4d882f85185de1a611e0fbf2357a4014b0d3b7ed
z-song <zosong@126.com>
2020-09-23 17:43:51 +0800

    wip

--- resources/views/partials/exception.blade.php
+++ resources/views/partials/exception.blade.php
@@ -1,14 +1,14 @@
 @if($errors->hasBag('exception') && config('app.debug') == true)
     <?php $error = $errors->getBag('exception');?>
-    <div class="alert alert-warning alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        <h4>
-            <i class="icon fa fa-warning"></i>
+    <div class="callout callout-danger">
+        <h5 class="text-danger">
+            <i class="icon fas fa-exclamation-triangle"></i>
             <i style="border-bottom: 1px dotted #fff;cursor: pointer;" title="{{ $error->first('type') }}" ondblclick="var f=this.innerHTML;this.innerHTML=this.title;this.title=f;">{{ class_basename($error->first('type')) }}</i>
             In <i title="{{ $error->first('file') }} line {{ $error->first('line') }}" style="border-bottom: 1px dotted #fff;cursor: pointer;" ondblclick="var f=this.innerHTML;this.innerHTML=this.title;this.title=f;">{{ basename($error->first('file')) }} line {{ $error->first('line') }}</i> :
-        </h4>
-        <p><a style="cursor: pointer;" onclick="$('#laravel-admin-exception-trace').toggleClass('hidden');$('i', this).toggleClass('fa-angle-double-down fa-angle-double-up');"><i class="fa fa-angle-double-down"></i>&nbsp;&nbsp;{!! $error->first('message') !!}</a></p>
+        </h5>
 
-        <p class="hidden" id="laravel-admin-exception-trace"><br>{!! nl2br($error->first('trace')) !!}</p>
+        <p><a style="cursor: pointer;" onclick="$('#laravel-admin-exception-trace').toggleClass('d-none');$('i', this).toggleClass('fa-angle-double-down fa-angle-double-up');"><i class="fa fa-angle-double-down"></i>&nbsp;&nbsp;{!! $error->first('message') !!}</a></p>
+
+        <p class="d-none" id="laravel-admin-exception-trace"><br>{!! nl2br($error->first('trace')) !!}</p>
     </div>
 @endif
