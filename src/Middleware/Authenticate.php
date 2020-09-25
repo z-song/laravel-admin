d576191446bd76ce61ec142895209fdc1024d853
z-song <zosong@126.com>
2020-09-25 15:26:20 +0800

    Fix #4987

--- src/Middleware/Authenticate.php
+++ src/Middleware/Authenticate.php
@@ -17,10 +17,12 @@ class Authenticate
      */
     public function handle($request, Closure $next)
     {
+        \config(['auth.defaults.guard' => 'admin']);
+
         $redirectTo = admin_base_path(config('admin.auth.redirect_to', 'auth/login'));
 
         if (Admin::guard()->guest() && !$this->shouldPassThrough($request)) {
-            return redirect()->guest($redirectTo);
+            return redirect()->to($redirectTo);
         }
 
         return $next($request);
