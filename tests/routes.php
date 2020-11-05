b081c18e838ddb33c5471fff2fbc7f785ebfc66f
Ben <ben@yocto.nu>
2020-11-05 16:12:22 +0100

    Fix StyleCI

--- tests/routes.php
+++ tests/routes.php
@@ -11,8 +11,8 @@ Route::group([
     'namespace'  => 'Tests\Controllers',
     'middleware' => ['web', 'admin'],
 ], static function () {
-	Route::resource('images', ImageController::class);
-	Route::resource('multiple-images', MultipleImageController::class);
-	Route::resource('files', FileController::class);
-	Route::resource('users', UserController::class);
+    Route::resource('images', ImageController::class);
+    Route::resource('multiple-images', MultipleImageController::class);
+    Route::resource('files', FileController::class);
+    Route::resource('users', UserController::class);
 });
