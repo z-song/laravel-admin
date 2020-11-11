f0f0a8e747f3fca5e69aa9f98d09097cf738a213
a443939412 <443939412@qq.com>
2020-11-11 18:16:24 +0800

    Grid filter 列过滤的reset按钮功能无效

--- src/Grid/Column/InputFilter.php
+++ src/Grid/Column/InputFilter.php
@@ -112,7 +112,7 @@ SCRIPT;
         <li class="text-right">
             <button class="btn btn-sm btn-flat btn-primary column-filter-submit pull-left" data-loading-text="{$this->trans('search')}..."><i class="fa fa-search"></i>&nbsp;&nbsp;{$this->trans('search')}</button>
             <button class="btn btn-sm btn-flat btn-default column-filter-all" data-loading-text="..."><i class="fa fa-undo"></i></button>
-			<span><a href="{$this->getFormAction()}" class="btn btn-sm btn-default btn-flat column-filter-all"><i class="fa fa-undo"></i></a></span>
+            <span><a href="{$this->getFormAction()}" class="btn btn-sm btn-default btn-flat column-filter-all"><i class="fa fa-undo"></i></a></span>
         </li>
     </ul>
     </form>
