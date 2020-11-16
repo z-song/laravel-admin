dcbfce7fd24269e3a39f8f9f0797ac1bbddbbf59
LukaS0lncev <47779140+LukaS0lncev@users.noreply.github.com>
2020-11-16 18:09:23 +0300

    Update title.blade.php
    Fixed demo link, there was an outdated link

--- resources/views/dashboard/title.blade.php
+++ resources/views/dashboard/title.blade.php
@@ -31,5 +31,5 @@
 <div class="links">
     <a href="https://github.com/z-song/laravel-admin" target="_blank">Github</a>
     <a href="http://laravel-admin.org/docs"  target="_blank">Documentation</a>
-    <a href="http://laravel-admin.org/demo"  target="_blank">Demo</a>
+    <a href="https://demo.laravel-admin.org/"  target="_blank">Demo</a>
 </div>
