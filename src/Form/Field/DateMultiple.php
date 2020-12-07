500c13867383ebdc338308c718fc1088fe7fb8cc
liangguifeng <1476982312@qq.com>
2020-12-07 18:33:55 +0800

    fix cdn to local

--- src/Form/Field/DateMultiple.php
+++ src/Form/Field/DateMultiple.php
@@ -12,7 +12,7 @@ class DateMultiple extends Text
 
     protected static $js = [
         'https://cdn.jsdelivr.net/npm/flatpickr',
-        'https://cdn.jsdelivr.net/npm/shortcut-buttons-flatpickr@0.1.0/dist/shortcut-buttons-flatpickr.min.js',
+        '/vendor/laravel-admin/shortcut-buttons-flatpickr/shortcut-buttons-flatpickr.min.js',
         'https://npmcdn.com/flatpickr@4.6.6/dist/l10n/zh.js',
 
     ];
