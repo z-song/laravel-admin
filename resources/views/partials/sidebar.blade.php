fc7edf85474f79ca939a33ae8502cd083f7b6447
2329697501 <2329697501@qq.com>
2021-03-04 15:52:41 +0800

    修改域名不指向public目录时后台首页logo显示错误
    修改域名不指向public目录时后台首页logo显示错误

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar elevation-4 sidebar-{{ config('admin.theme.sidebar') }}">
 
     <a href="{{ admin_url('/') }}" class="brand-link {{ config('admin.theme.logo') ? 'navbar-'.config('admin.theme.logo') : '' }}">
-        <img src="{!! config('admin.logo.image') !!}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{!! admin_asset(config('admin.logo.image')) !!}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">{!! config('admin.logo.text', config('admin.name')) !!}</span>
     </a>
 
