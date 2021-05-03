4081737977585574dcd083f4c22d3a9c68fa6d1f
jxlwqq <jxlwqq@gmail.com>
2021-05-03 21:41:11 +0800

    Update config/admin.php

--- config/admin.php
+++ config/admin.php
@@ -223,7 +223,7 @@ return [
          * or specific method to path like: get:admin/auth/logs.
          */
         'except' => [
-            env('ADMIN_ROUTE_PREFIX', 'admin') . '/auth/logs*',
+            env('ADMIN_ROUTE_PREFIX', 'admin').'/auth/logs*',
         ],
     ],
 
