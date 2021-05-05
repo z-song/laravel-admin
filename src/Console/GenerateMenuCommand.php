f979cd568fb0808ca18714f0c686f72964c01691
Andreas Asatera <tolong@buatin.website>
2021-05-05 13:56:01 +0700

    Exclude route from generate menu command

--- src/Console/GenerateMenuCommand.php
+++ src/Console/GenerateMenuCommand.php
@@ -55,7 +55,8 @@ class GenerateMenuCommand extends Command
                 && !Str::startsWith($uri, 'admin/auth/')
                 && !Str::endsWith($uri, '/create')
                 && !Str::contains($uri, '{')
-                && in_array('GET', $route->methods());
+                && in_array('GET', $route->methods())
+                && !in_array(substr($route->uri(), strlen('admin/')), config('admin.menu_exclude'));
         })
             ->map(function (Route $route) {
                 $uri = substr($route->uri(), strlen('admin/'));
