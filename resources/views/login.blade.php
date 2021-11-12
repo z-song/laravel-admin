2bc9060f69af7e3cbf7ab55f84356a43c30c54ed
刘军 <995672751@qq.com>
2021-11-12 10:59:23 +0800

    去除空格
    去除空格，兼容有些浏览器无法自动解析

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -85,7 +85,7 @@
 <!-- /.login-box -->
 
 <!-- jQuery 2.1.4 -->
-<script src="{{ admin_asset("vendor/laravel-admin/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js")}} "></script>
+<script src="{{ admin_asset("vendor/laravel-admin/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js")}}"></script>
 <!-- Bootstrap 3.3.5 -->
 <script src="{{ admin_asset("vendor/laravel-admin/AdminLTE/bootstrap/js/bootstrap.min.js")}}"></script>
 <!-- iCheck -->
