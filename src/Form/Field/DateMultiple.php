1f06ce599daeece5a1aa371bf3c71b380b0e173a
fasthold <wc5d@hotmail.com>
2022-02-15 10:33:00 +0800

    JSdelivr CDN is blocked in some area
    Use npmcdn instead

--- src/Form/Field/DateMultiple.php
+++ src/Form/Field/DateMultiple.php
@@ -5,14 +5,14 @@ namespace Encore\Admin\Form\Field;
 class DateMultiple extends Text
 {
     protected static $css = [
-        'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
-        'https://cdn.jsdelivr.net/npm/shortcut-buttons-flatpickr@0.3.0/dist/themes/light.min.css',
+        'https://npmcdn.com/flatpickr/dist/flatpickr.min.css',
+        'https://npmcdn.com/shortcut-buttons-flatpickr@0.3.0/dist/themes/light.min.css',
 
     ];
 
     protected static $js = [
-        'https://cdn.jsdelivr.net/npm/flatpickr',
-        'https://cdn.jsdelivr.net/npm/shortcut-buttons-flatpickr@0.1.0/dist/shortcut-buttons-flatpickr.min.js',
+        'https://npmcdn.com/flatpickr',
+        'https://npmcdn.com/shortcut-buttons-flatpickr@0.1.0/dist/shortcut-buttons-flatpickr.min.js',
         'https://npmcdn.com/flatpickr@4.6.6/dist/l10n/zh.js',
 
     ];
