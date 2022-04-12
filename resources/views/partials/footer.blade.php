ed930f24b54808f811e462f27e6288dfd28c17c7
MTM <m5taha5m@gmail.com>
2022-04-12 15:13:31 +0430

    feat: Add custom footer-view config

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,18 +1,22 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        @if(config('admin.show_environment'))
-            <strong>Env</strong>&nbsp;&nbsp; {!! config('app.env') !!}
-        @endif
+    @if(empty(config('admin.footer_view')))
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+            @if(config('admin.show_environment'))
+                <strong>Env</strong>&nbsp;&nbsp; {!! config('app.env') !!}
+            @endif
 
-        &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
 
-        @if(config('admin.show_version'))
-        <strong>Version</strong>&nbsp;&nbsp; {!! \Encore\Admin\Admin::VERSION !!}
-        @endif
+            @if(config('admin.show_version'))
+                <strong>Version</strong>&nbsp;&nbsp; {!! \Encore\Admin\Admin::VERSION !!}
+            @endif
 
-    </div>
-    <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+        </div>
+        <!-- Default to the left -->
+        <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    @else
+        @include(config('admin.footer_view'))
+    @endif
 </footer>
