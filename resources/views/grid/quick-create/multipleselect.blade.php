12e994c75352c0566ad80bb47ca77895991d8b3c
Sin of Sloth <1354709653@qq.com>
2022-05-05 16:01:58 +0800

    修复表格快速创建多选框bug

--- resources/views/grid/quick-create/multipleselect.blade.php
+++ resources/views/grid/quick-create/multipleselect.blade.php
@@ -8,7 +8,7 @@
 </style>
 
 <div class="input-group input-group-sm">
-    <select class="form-control {{$class}}" style="width: 100%;" name="{{$name}}" {!! $attributes !!} multiple data-placeholder="{{ $label }}">
+    <select class="form-control {{$class}}" style="width: 100%;" name="{{$name}}[]" {!! $attributes !!} multiple data-placeholder="{{ $label }}">
 
         <option value=""></option>
         @foreach($options as $select => $option)
