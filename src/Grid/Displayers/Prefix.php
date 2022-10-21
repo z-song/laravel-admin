85ee031e8be39661d6bc789403b873c18f3c942c
MTM <m5taha5m@gmail.com>
2022-10-21 19:58:54 +0330

    refactor: Don't use &nbsp; where possible

--- src/Grid/Displayers/Prefix.php
+++ src/Grid/Displayers/Prefix.php
@@ -4,7 +4,7 @@ namespace Encore\Admin\Grid\Displayers;
 
 class Prefix extends AbstractDisplayer
 {
-    public function display($prefix = null, $delimiter = '&nbsp;')
+    public function display($prefix = null, $delimiter = '')
     {
         if ($prefix instanceof \Closure) {
             $prefix = $prefix->call($this->row, $this->getValue(), $this->getColumn()->getOriginal());
