8ac85b37f9a7004b8e2a5af51c8f42a3f774a529
Ang Kosal <angkosal@gmail.com>
2024-02-27 14:49:12 +0700

    Comment power by

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     <!-- To the right -->
     <div class="pull-right hidden-xs">
         @if(config('admin.show_environment'))
-            <strong>Env</strong>&nbsp;&nbsp; {!! config('app.env') !!}
+        <strong>Env</strong>&nbsp;&nbsp; {!! config('app.env') !!}
         @endif
 
         &nbsp;&nbsp;&nbsp;&nbsp;
@@ -14,5 +14,5 @@
 
     </div>
     <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    {{--<strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>--}}
 </footer>
